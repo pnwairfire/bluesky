973cc06d39903d784676c6cfd21def5bd75ecbee
Joel Dubowy <jdubowy@gmail.com>
2015-02-20 16:22:35 -0800

    Implement simple BlueSky modules as python modules rather than sub-packages
    More complicated BlueSky modules can still be implemented as
    sub-packages within the bluesky.modules package

--- bluesky/modules/__init__.py
+++ bluesky/modules/__init__.py
@@ -1,4 +1,10 @@
 """bluesky.modules
+
+This subpackage contains BlueSky modules.  These modules can be implemented
+either as python modules or subpackages. All that's required is that the
+modules/packages have a run method that can be imported.  (So, if implemented
+as a subpackage, the __init__.py just needs to define a run method or import
+it from one of it's modules.)
 """
 
 __author__      = "Joel Dubowy"
