7498b0ce40a40b0604c9c9225eb153f4e4f373b3
Joel Dubowy <jdubowy@gmail.com>
2015-02-23 16:54:14 -0800

    Fix import

--- bluesky/modules/emissions.py
+++ bluesky/modules/emissions.py
@@ -6,7 +6,7 @@ __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
 
 import logging
 
-import emitcalc
+from emitcalc.calculator import EmissionsCalculator
 from fccs2ef.lookup import Fccs2Ef
 
 __all__ = [
@@ -15,7 +15,7 @@ __all__ = [
 
 def run(fires):
     logging.debug("Running emissions module (NOOP)")
-    calculator = emitcalc.EmissionsCalculator(Fccs2Ef())
+    calculator = EmissionsCalculator(Fccs2Ef())
     for fire in fires:
         for fb in fire.fuelbeds:
             fb['emissions'] = calculator.calculate(fb["fccs_id"], fb["consumption"])
