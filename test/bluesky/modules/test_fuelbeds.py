93a6714dd3f6693d725833e0af07a804dd049433
Joel Dubowy <jdubowy@gmail.com>
2015-03-20 18:06:36 -0700

    WIP adding to and updated fulbed module unit test

--- test/bluesky/modules/test_fuelbeds.py
+++ test/bluesky/modules/test_fuelbeds.py
@@ -4,17 +4,155 @@ from py.test import raises
 
 from bluesky.modules import fuelbeds
 
-class TestEstimatorGetFromShape:
-    pass
+PERIMETER = {
+    "type": "MultiPolygon",
+    "coordinates": [
+        [
+            [
+                [-84.8194, 30.5222],
+                [-84.8197, 30.5209],
+                ...
+                [-84.8193, 30.5235],
+                [-84.8194, 30.5222]
+            ]
+        ]
+    ]
+}
 
-class TestEstimatorGetFromLatLng:
-    pass
+## Valid fuelbed info
+
+FUELBED_INFO_60_40 = {
+    "fuelbeds": {
+        "46": {
+            "grid_cells": 6, "percent": 60.0
+        },
+        "47": {
+            "grid_cells": 4, "percent": 40.0
+        }
+    },
+    "units": "m^2",
+    "grid_cells": 5,
+    "area": 4617927.854331356
+}
+
+FUELBED_INFO_40_30_26_6 = {
+    "fuelbeds": {
+        "46": {
+            "grid_cells": 8, "percent": 40.0
+        },
+        "47": {
+            "grid_cells": 6, "percent": 30.0
+        },
+        "48": {
+            "grid_cells": 5, "percent": 26.0
+        },
+        "49": {
+            "grid_cells": 1, "percent": 4.0
+        }
+    },
+    "units": "m^2",
+    "grid_cells": 5,
+    "area": 4617927.854331356
+}
+
+## Invalid fuelbed info
+
+# total % < 100
+FUELBED_INFO_60_30 = copy.deepcopy(FUELBED_INFO_60_40)
+FUELBED_INFO_60_30['fuelbeds']['47']['percent'] = 30
+# total % > 100
+FUELBED_INFO_60_40_10 = copy.deepcopy(FUELBED_INFO_60_40)
+FUELBED_INFO_60_40_10['fuelbeds']['50'] = {"grid_cells": 1, "percent": 10.0}
+
+##
+## Tests for Estimator.estimate
+##
+
+##
+## Tests for Estimator.estimate
+##
+
+class TestEstimatorInsufficientDataForLookup:
+
+    def setup(self):
+        lookup = mock.Mock()
+        self.estimator = fuelbeds.Estimator(lookup)
+
+    def test_no_perimeter_or_lat_or_lng(self):
+        pass
+
+class BaseTestEstimatorEstimate:
+    """Base class for testing Estimator.estimate
+    """
+
+    def setup(self):
+        lookup = mock.Mock()
+        self.estimator = fuelbeds.Estimator(lookup)
+
+    # Tests of invalid lookup data
+
+    def test_none_lookup_info(self):
+        self.estimator.lookup.look_up = lambda p: None
+        with raises(RuntimeError) as e:
+            self.estimator.estimate(self.fire)
+
+    def test_empty_lookup_info(self):
+        self.estimator.lookup.look_up = lambda p: {}
+        with raises(RuntimeError) as e:
+            self.estimator.estimate(self.fire)
+
+    def test_lookup_info_percentages_less_than_100(self):
+        self.estimator.lookup.look_up = lambda p: FUELBED_INFO_60_30
+        with raises(RuntimeError) as e:
+            self.estimator.estimate(self.fire)
+
+    def test_lookup_info_percentages_greater_than_100(self):
+        self.estimator.lookup.look_up = lambda p: FUELBED_INFO_60_40_10
+        with raises(RuntimeError) as e:
+            self.estimator.estimate(self.fire)
+
+    # Test of valid lookup data
+
+    def test_no_truncation(self):
+        self.estimator.lookup.look_up = lambda p: FUELBED_INFO_60_40
+        expected_fuelbeds = [
+            {'fccs_id': '46', 'pct': 60},
+            {'fccs_id': '47', 'pct': 40}
+        ]
+        # Having 'perimeter' key will trigger call to self.estimator.lookup.look_up;
+        # The value of PERIMETER is not actually used here
+        self.estimator.estimate(self.fire)
+        assert expected == self.fire.get('fuelbeds')
+
+    def test_with_truncation(self):
+        # TODO: implement
+        pass
+
+class TestEstimatorGetFromPerimeter(BaseTestEstimatorEstimate):
+    def setup(self):
+        self.fire = {"perimeter": PERIMETER}
+        super(TestEstimatorGetFromPerimeter, self).setup()
+
+
+class TestEstimatorGetFromLatLng(BaseTestEstimatorEstimate):
+
+    def setup(self):
+        self.fire = {"latitude": 46.0, 'longitude': -120.34}
+        super(TestEstimatorGetFromLatLng, self).setup()
+
+##
+## Tests for Estimator._truncate
+##
 
 class TestEstimatorTruncation:
 
     def setup(self):
-        fire = mock.Mock()
-        self.estimator = fuelbeds.Estimator(fire)
+        lookup = mock.Mock()
+        self.estimator = fuelbeds.Estimator(lookup)
+
+
+    # TDOO: UPDATE ALL THESE TESTS TO USE NEW CLASS INTERFACE
+
 
     def test_truncate_empty_set(self):
         self.estimator.fire.fuelbeds = []
@@ -73,3 +211,19 @@ class TestEstimatorTruncation:
             {'fccs_id': 5, 'pct': 16}
         ]
         assert expected == self.estimator.fire.fuelbeds
+
+##
+## Tests for Estimator._adjust_percentages
+##
+
+class TestEstimatorPercentageAdjustment:
+
+    def setup(self):
+        lookup = mock.Mock()
+        self.estimator = fuelbeds.Estimator(lookup)
+
+    def test_no_adjustment(self):
+        pass
+
+    def test_with_adjustment(self):
+        pass
