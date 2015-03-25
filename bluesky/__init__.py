b17c47d824bdab785eea4705f40531f4debf103c
Joel Dubowy <jdubowy@gmail.com>
2015-03-24 17:22:24 -0700

    Define version in bluesky's __init__ and bump to 0.1.1

--- bluesky/__init__.py
+++ bluesky/__init__.py
@@ -1,5 +1,8 @@
 """bluesky
 """
 
+__version_info__ = (0,1,1)
+__version__ = '.'.join([str(n) for n in __version_info__])
+
 __author__      = "Joel Dubowy"
 __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import re
 from distutils.core import setup
 
+from bluesky import __version__
+
 # Note: using pip.req.parse_requirements like so:
 #  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
 # results in the folloing error on Heroku:
@@ -26,7 +28,7 @@ test_requirements = parse_requirements(dependency_links, 'requirements-test.txt'
 
 setup(
     name='BlueSky Pipeline',
-    version='0.1.0',
+    version=__version__,
     author='Joel Dubowy',
     author_email='jdubowy@gmail.com',
     packages=[
