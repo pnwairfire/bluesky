ac8be0a544da2f65ee3d1fe53ddde595ebc5eb43
Joel Dubowy <jdubowy@gmail.com>
2015-03-25 08:15:55 -0700

    Fix specification of github dependencies

--- setup.py
+++ setup.py
@@ -14,10 +14,15 @@ def parse_requirements(dep_links, req_file_name):
         for r in f.read().splitlines():
             m = git_url_matcher.match(r)
             if m:
-                # TODO: figure out how dep link should be formatted
-                dep_links.append('%s/%s@v%s#egg=%s-%s' % (
-                    m.group(1), m.group(2), m.group(4), m.group(2), m.group(4)))
-                reqs.append("%s==%s" % (m.group(2), m.group(4)))
+                # When there's a dependency from github.com (or other git
+                # server), we need to add a dependency link that points to
+                # a downloadable tarball or zip file and then add the package
+                # name (without version specified) to the install_requires
+                # see http://peak.telecommunity.com/DevCenter/setuptools#dependencies-that-aren-t-in-pypi
+                # TODO: test to see if this url format works for bitbucket as well
+                dep_links.append('%s/%s/tarball/v%s#egg=%s' % (
+                    m.group(1), m.group(2), m.group(4), m.group(2)))
+                reqs.append("%s" % (m.group(2)))
             else:
                 reqs.append(r)
         return reqs
