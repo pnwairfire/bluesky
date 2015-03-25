234ba8b10b0e873e0913b848016f4ed793a2ce1d
Joel Dubowy <jdubowy@gmail.com>
2015-03-24 17:11:59 -0700

    Refactor dependency parsing and setting to try to get depencies installed correctly when using pip

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
+import re
 from distutils.core import setup
 
 # Note: using pip.req.parse_requirements like so:
 #  > REQUIREMENTS = [str(ir.req) for ir in parse_requirements('requirements.txt')]
 # results in the folloing error on Heroku:
 #    TypeError: parse_requirements() missing 1 required keyword argument: 'session'
-git_url_matcher = re.compile('^git\+.+/([^/.]+)(\.git)?@v?([0-9.]+)$')
+git_url_matcher = re.compile('^git\+(.+)/([^/.]+)(\.git)?@v?([0-9.]+)$')
 def parse_requirements(dep_links, req_file_name):
     with open(req_file_name) as f:
         reqs = []
         for r in f.read().splitlines():
             m = git_url_matcher.match(r)
             if m:
-                dep_links.append(r)
-                reqs.append("%s==%s" % (m.group(1), m.group(3)))
+                # TODO: figure out how dep link should be formatted
+                dep_links.append('%s/%s@v%s#egg=%s-%s' % (
+                    m.group(1), m.group(2), m.group(4), m.group(2), m.group(4)))
+                reqs.append("%s==%s" % (m.group(2), m.group(4)))
             else:
                 reqs.append(r)
         return reqs
