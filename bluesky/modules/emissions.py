dccd1e74cf8aa774b89578c7a60fc2fc07894e63
Joel Dubowy <jdubowy@gmail.com>
2015-03-31 14:47:15 -0700

    Finish updating emissions module to work with new version of emitcalc

--- bluesky/modules/emissions.py
+++ bluesky/modules/emissions.py
@@ -8,7 +8,7 @@ import logging
 
 from emitcalc.calculator import EmissionsCalculator
 from eflookup.fccs2ef.lookup import Fccs2Ef
-from eflookup.fepsef.lookup import FepsEFLookup
+from eflookup.fepsef import FepsEFLookup
 
 __all__ = [
     'run'
@@ -27,27 +27,24 @@ def run(fires, config=None):
     else:
         _run_feps(fires)
 
-def _run_feps(self):
+def _run_feps(fires):
     logging.debug("Running emissions module FEPS EFs")
 
     # The same lookup object is used for both Rx and WF
-    feps_ef_lookup = FepsEFLookup()
-
+    calculator = EmissionsCalculator(FepsEFLookup())
     for fire in fires:
         for fb in fire.fuelbeds:
-            fb['emissions'] = calculator.calculate([feps_ef_lookup],
-                fb["consumption"], is_rx)
+            fb['emissions'] = calculator.calculate(fb["consumption"])
 
 def _run_urbanski(fires):
     logging.debug("Running emissions module with Urbanski EFs")
 
     # Instantiate two lookup object, one Rx and one WF, to be reused
-    fccs2ef_wf = Fccs2Ef()
+    fccs2ef_wf = Fccs2Ef(is_rx=False)
     fccs2ef_rx = Fccs2Ef(is_rx=True)
 
-    calculator = EmissionsCalculator()
     for fire in fires:
         fccs2ef = fccs2ef_rx if fire.get('type') == "rx" else fccs2ef_wf
         for fb in fire.fuelbeds:
-            fb['emissions'] = calculator.calculate([fccs2ef[fb["fccs_id"]]],
-                fb["consumption"], is_rx)
+            calculator = EmissionsCalculator([fccs2ef[fb["fccs_id"]]])
+            fb['emissions'] = calculator.calculate(fb["consumption"])
