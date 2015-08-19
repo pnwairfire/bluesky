bf2b6dbdb3c4901ba5cfdef743f79bba835ac88c
Joel Dubowy <jdubowy@gmail.com>
2015-08-19 16:22:38 -0700

    Fix docstrings

--- bluesky/models/__init__.py
+++ bluesky/models/__init__.py
@@ -1,5 +1,4 @@
-"""bluesky.models
-"""
+"""bluesky.models"""
 
 __author__      = "Joel Dubowy"
 __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
--- bluesky/models/fires.py
+++ bluesky/models/fires.py
@@ -1,5 +1,4 @@
-"""tasks.takedown - fabric tasks for ....
-"""
+"""bluesky.models.fires"""
 
 __author__      = "Joel Dubowy"
 __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
