2faaaeea190db15df65b3be47c3c55f4cb07aa91
Joel Dubowy <jdubowy@gmail.com>
2015-09-15 16:30:27 -0700

    Put web app main function in bluesky.web.app

--- bluesky/web/app.py
+++ bluesky/web/app.py
@@ -6,11 +6,19 @@ __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
 import tornado.ioloop
 import tornado.web
 
+from . import config
 from .api.v1.run import Run
-application = tornado.web.Application([
-    (r"/api/v1/run/", Run),
-])
 
-if __name__ == '__main__':
-    application.listen(8888)
+routes = [
+    (r"/api/v1/run/", Run),
+]
+application = tornado.web.Application(routes, debug=config.DEBUG)
+
+tornado
+setup_logger(application)
+
+def main():
+    application.listen(
+        8888
+    )
     tornado.ioloop.IOLoop.current().start()
