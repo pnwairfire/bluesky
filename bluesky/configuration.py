f6377570988b987c2c77c2b48b98a6a524a63ad2
Joel Dubowy <jdubowy@gmail.com>
2015-09-23 10:54:10 -0700

    Refactor get_config_value to work with arbitrarily nested config dict
    Includes unit tests

--- bluesky/configuration.py
+++ bluesky/configuration.py
@@ -6,6 +6,8 @@ __copyright__   = "Copyright 2015, AirFire, PNW, USFS"
 import ConfigParser
 
 __all__ = [
+    'config_parser_from_dict',
+    'config_parser_to_dict',
     'get_config_value'
 ]
 
@@ -54,10 +56,16 @@ def config_parser_to_dict(config):
 ## Utility mehtods
 ##
 
-def get_config_value(config, section, key, default=None):
-    if config:
-        try:
-            return config.get(section, key)
-        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-            pass
+def get_config_value(config, *keys, **kwargs):
+    """Returns value from arbitrary nesting in config dict
+
+    Recognized kwargs:
+     - default -- if not specified, None is used as the default
+    """
+    default = kwargs.get('default', None)
+    if config and isinstance(config, dict) and keys:
+        if len(keys) == 1:
+            return config.get(keys[0], default)
+        elif keys[0] in config:
+            return get_config_value(config[keys[0]], *keys[1:], **kwargs)
     return default
--- test/bluesky/test_configuration.py
+++ test/bluesky/test_configuration.py
@@ -7,7 +7,11 @@ import ConfigParser
 
 from py.test import raises
 
-from bluesky import configuration
+from bluesky.configuration import (
+    config_parser_from_dict,
+    config_parser_to_dict,
+    get_config_value
+)
 
 
 ##
@@ -17,22 +21,22 @@ from bluesky import configuration
 class TestConfigParserFromDict(object):
 
     def test_none(self):
-        c = configuration.config_parser_from_dict(None)
+        c = config_parser_from_dict(None)
         assert isinstance(c, ConfigParser.ConfigParser)
         assert [] == c.sections()
 
     def test_empty(self):
-        c = configuration.config_parser_from_dict({})
+        c = config_parser_from_dict({})
         assert isinstance(c, ConfigParser.ConfigParser)
         assert [] == c.sections()
 
     def test_invalid(self):
         with raises(ValueError) as e:
-            c = configuration.config_parser_from_dict("sdf")
+            c = config_parser_from_dict("sdf")
             # TODO: assert e's message
 
         with raises(ValueError) as e:
-            c = configuration.config_parser_from_dict({'s': "sdf"})
+            c = config_parser_from_dict({'s': "sdf"})
             # TODO: assert e's message
 
     def test_valid(self):
@@ -40,7 +44,7 @@ class TestConfigParserFromDict(object):
             'a': { 'aa': 'sdf', 'ab': "343"},
             'b': { 'ba': "123", 'bb': 'sdfs'}
         }
-        c = configuration.config_parser_from_dict(d)
+        c = config_parser_from_dict(d)
         assert isinstance(c, ConfigParser.ConfigParser)
         assert ['a','b'] == c.sections()
         assert 'sdf' == c.get('a', 'aa')
@@ -51,11 +55,11 @@ class TestConfigParserFromDict(object):
 class TestConfigParserToDict(object):
 
     def test_none(self):
-        d = configuration.config_parser_to_dict(None)
+        d = config_parser_to_dict(None)
         assert {} == d
 
     def test_empty(self):
-        d = configuration.config_parser_to_dict(ConfigParser.ConfigParser())
+        d = config_parser_to_dict(ConfigParser.ConfigParser())
         assert {} == d
 
     def test_valid(self):
@@ -70,4 +74,56 @@ class TestConfigParserToDict(object):
             'a': { 'aa': 'sdf', 'ab': "343"},
             'b': { 'ba': "123", 'bb': 'sdfs'}
         }
-        assert d == configuration.config_parser_to_dict(c)
+        assert d == config_parser_to_dict(c)
+
+##
+## Tests for utilitu methods
+##
+
+class TestGetConfigValue(object):
+    def test_no_config(self):
+        assert None == get_config_value(None, 's')
+        assert None == get_config_value(None, 's', 'sdf')
+        assert 1 == get_config_value(None, 's', default=1)
+        assert 1 == get_config_value(None, 's', 'sdf', default=1)
+
+    def test_empty_config(self):
+        assert None == get_config_value({}, 's')
+        assert None == get_config_value({}, 's', 'sdf')
+        assert 1 == get_config_value({}, 's', default=1)
+        assert 1 == get_config_value({}, 's', 'sdf', default=1)
+
+    def test_full(self):
+        config = {
+            'a': {
+                'aa': 'sdf',
+                'ab': 343
+            },
+            'b': {
+                'ba': 123,
+                'bb': 'sdfs',
+                'cc': {
+                    'ccc': "SDF"
+                }
+            }
+        }
+
+        assert None == get_config_value(config, 'z')
+        assert None == get_config_value(config, 'z', 'b')
+        assert None == get_config_value(config, 'a', 'b')
+        assert None == get_config_value(config, 'b', 'cc', 'z')
+        assert None == get_config_value(config, 'a', 'aa', 'z')
+        assert 1 == get_config_value(config, 'z', default=1)
+        assert 1 == get_config_value(config, 'z', 'b', default=1)
+        assert 1 == get_config_value(config, 'a', 'b', default=1)
+        assert 1 == get_config_value(config, 'b', 'cc', 'z', default=1)
+        assert 1 == get_config_value(config, 'a', 'aa', 'z', default=1)
+
+        assert {'aa': 'sdf','ab': 343} == get_config_value(config, 'a')
+        assert {'aa': 'sdf','ab': 343} == get_config_value(config, 'a', default=1)
+        assert 'sdf' == get_config_value(config, 'a', 'aa')
+        assert 'sdf' == get_config_value(config, 'a', 'aa', default=1)
+        assert 'sdf' == get_config_value(config['a'], 'aa')
+        assert 'sdf' == get_config_value(config['a'], 'aa', default=1)
+        assert 'SDF' == get_config_value(config, 'b', 'cc', 'ccc')
+        assert 'SDF' == get_config_value(config, 'b', 'cc', 'ccc', default=1)
