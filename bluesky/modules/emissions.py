31849d3f9829f9ff6eaada19cb8d47475e42ea10
Joel Dubowy <jdubowy@gmail.com>
2015-09-24 11:00:23 -0700

    Support species whitelist in emissions module

--- bluesky/modules/emissions.py
+++ bluesky/modules/emissions.py
@@ -11,7 +11,6 @@ from eflookup.fccs2ef.lookup import Fccs2Ef
 from eflookup.fepsef import FepsEFLookup
 
 from bluesky import datautils
-from bluesky.configuration import get_config_value
 from bluesky.exceptions import BlueSkyConfigurationError
 
 __all__ = [
@@ -26,31 +25,35 @@ def run(fires_manager):
      - fires_manager -- bluesky.models.fires.FiresManager object
     """
     efs = fires_manager.get_config_value('emissions', 'efs', default='feps').lower()
+    species = fires_manager.get_config_value('emissions', 'species', default=[])
     fires_manager.processed(__name__, __version__,
         emitcalc_version=emitcalc.__version__, ef_set=efs)
     if efs == 'urbanski':
-        _run_urbanski(fires_manager)
+        _run_urbanski(fires_manager, species)
     elif efs == 'feps':
-        _run_feps(fires_manager)
+        _run_feps(fires_manager, species)
     else:
         raise BlueSkyConfigurationError(
             "Invalid emissions factors set: '{}'".format(efs))
-    fires_manager.summarize(emissions=datautils.summarize(fires_manager.fires, 'emissions'))
+    fires_manager.summarize(emissions=datautils.summarize(
+        fires_manager.fires, 'emissions'))
 
-def _run_feps(fires_manager):
+def _run_feps(fires_manager, species):
     logging.debug("Running emissions module FEPS EFs")
 
     # The same lookup object is used for both Rx and WF
-    calculator = EmissionsCalculator(FepsEFLookup())
+    calculator = EmissionsCalculator(FepsEFLookup(), species=species)
     for fire in fires_manager.fires:
         if 'fuelbeds' not in fire:
-            raise ValueError("Missing fuelbed data required for computing emissions")
+            raise ValueError(
+                "Missing fuelbed data required for computing emissions")
         for fb in fire.fuelbeds:
             if 'consumption' not in fb:
-                raise ValueError("Missing consumption data required for computing emissions")
+                raise ValueError(
+                    "Missing consumption data required for computing emissions")
             fb['emissions'] = calculator.calculate(fb["consumption"])
 
-def _run_urbanski(fires_manager):
+def _run_urbanski(fires_manager, species):
     logging.debug("Running emissions module with Urbanski EFs")
 
     # Instantiate two lookup object, one Rx and one WF, to be reused
@@ -59,10 +62,13 @@ def _run_urbanski(fires_manager):
 
     for fire in fires_manager.fires:
         if 'fuelbeds' not in fire:
-            raise ValueError("Missing fuelbed data required for computing emissions")
+            raise ValueError(
+                "Missing fuelbed data required for computing emissions")
         fccs2ef = fccs2ef_rx if fire.get('type') == "rx" else fccs2ef_wf
         for fb in fire.fuelbeds:
             if 'consumption' not in fb:
-                raise ValueError("Missing consumption data required for computing emissions")
-            calculator = EmissionsCalculator([fccs2ef[fb["fccs_id"]]])
+                raise ValueError(
+                    "Missing consumption data required for computing emissions")
+            calculator = EmissionsCalculator([fccs2ef[fb["fccs_id"]]],
+                species=species)
             fb['emissions'] = calculator.calculate(fb["consumption"])
