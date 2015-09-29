a8d368b6f06fdc41781fa3499e29e115e7f0b19c
Joel Dubowy <jdubowy@gmail.com>
2015-09-29 15:57:31 -0700

    Add skeleton modules for plumerise and localmet
    they currently raise NotImplementedError exceptions

---
+++ bluesky/modules/localmet.py
@@ -0,0 +1,21 @@
+"""bluesky.modules.localmet"""
+
+__author__      = "Joel Dubowy"
+__copyright__   = "Copyright 2015, AirFire, PNW, USFS"
+
+import consume
+import logging
+
+__all__ = [
+    'run'
+]
+
+def run(fires_manager, config=None):
+    """Runs plumerise module
+
+    Args:
+     - fires_manager -- bluesky.models.fires.FiresManager object
+    Kwargs:
+     - config -- optional configparser object
+    """
+    raise NotImplementedError("localmet not yet implemented")
