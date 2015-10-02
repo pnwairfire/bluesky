da53d3ac4e883e0a00f009862e19d66f663a646f
Joel Dubowy <jdubowy@gmail.com>
2015-10-02 16:20:38 -0700

    Add TODOs in dispersion module

--- bluesky/modules/dispersion.py
+++ bluesky/modules/dispersion.py
@@ -27,4 +27,10 @@ def run(fires_manager, config=None):
     Kwargs:
      - config -- optional configparser object
     """
+    # TODO: Create a separate hysplit wrapper module.  If config > dispersion
+    #  > module (?) specifies hysplit, use the hysplit wrapper module, which will
+    # produce input files for hysplit, call hysplit, parse it's output (or not?), and
+    # then add information to fires_manager indicating where to find the hysplit output
+    # (not sure if hysplit will be called per fire or on all fires at once).
+    # TODO: support VSMOKE as well
     raise NotImplementedError("dispersion not yet implemented")
