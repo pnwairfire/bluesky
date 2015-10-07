957289e70d4996c451f74ad6569a16ce6fdb4a7f
Joel Dubowy <jdubowy@gmail.com>
2015-10-07 11:10:16 -0700

    Add skeleton unit test module for consumption

---
+++ test/bluesky/modules/test_consumption.py
@@ -0,0 +1,15 @@
+"""Unit tests for bluesky.modules.consumption"""
+
+__author__      = "Joel Dubowy"
+__copyright__   = "Copyright 2015, AirFire, PNW, USFS"
+
+import copy
+import mock
+
+from py.test import raises
+
+from bluesky.modules import consumption
+
+
+class FuelLoadingsManagerTest(object):
+    pass
