529af2c4b0d172b1efe39717ee398f7d2867c59c
Joel Dubowy <jdubowy@gmail.com>
2015-10-26 13:33:57 -0700

    Add skeleton 'export' module
    raises NotImplementedError

---
+++ bluesky/modules/export.py
@@ -0,0 +1,18 @@
+"""bluesky.modules.export"""
+
+__author__      = "Joel Dubowy"
+__copyright__   = "Copyright 2015, AirFire, PNW, USFS"
+
+__all__ = [
+    'run'
+]
+
+__version__ = "0.1.0"
+
+def run(fires_manager):
+    """runs the export module
+
+    Args:
+     - fires_manager -- bluesky.models.fires.FiresManager object
+    """
+    raise NotImplementedError("Bluesky 'export' module not yet implemented")
