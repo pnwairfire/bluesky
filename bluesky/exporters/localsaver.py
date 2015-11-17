281dcfb4254e7c720c1e92fe59ba7aa24a0d0fc6
Joel Dubowy <jdubowy@gmail.com>
2015-11-17 14:34:14 -0800

    Add note and remove todo

--- bluesky/exporters/localsaver.py
+++ bluesky/exporters/localsaver.py
@@ -23,10 +23,10 @@ class LocalSaveExporter(ExporterBase):
         if not self._dest:
             raise BlueSkyConfigurationError("Specify destination "
                 "('config' > 'export' > 'localsave' > 'dest_dir')")
+        # Note: _bundle will create self._dest if it doesn't exist
 
     def export(self, fires_manager):
         logging.info('Saving locally to %s', self._dest)
-        # TODO: move d to self._dest
         return {
             'directory': self._bundle(fires_manager, self._dest)
         }
