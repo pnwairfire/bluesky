08479695f7a2128fb6b6bec1df8530b6bc2b5713
Joel Dubowy <jdubowy@gmail.com>
2015-12-08 18:36:47 -0800

    Reorganize code a bit

--- bluesky/modules/dispersion.py
+++ bluesky/modules/dispersion.py
@@ -31,44 +31,16 @@ def run(fires_manager):
         default='hysplit').lower()
     processed_kwargs = {}
     try:
-        module_name = "bluesky.dispersers.{}".format(model)
-        logging.debug("Importing %s", module_name)
-        try:
-            module = importlib.import_module(module_name)
-        except ImportError:
-            raise BlueSkyConfigurationError(
-                "Invalid dispersion model: '{}'".format(model))
-
-        klass_name = "{}Dispersion".format(model.upper())
-        logging.debug("Loading class %s", klass_name)
-        try:
-            klass = getattr(module, klass_name)
-        except:
-            # TODO: use more appropriate exception class
-            raise RuntimeError("{} does not define class {}".format(
-                module_name, klass_name))
-
+        module, klass = _get_module_and_class(model)
         model_config = fires_manager.get_config_value(
             'dispersion', model, default={})
-
         disperser = klass(fires_manager.met, **model_config)
         processed_kwargs.update({
             "{}_version".format(model): module.__version__
         })
 
-        start_str = fires_manager.get_config_value('dispersion', 'start')
-        num_hours = fires_manager.get_config_value('dispersion', 'num_hours')
-        if not start_str or not num_hours:
-            raise ValueError("Config settings 'start' and 'num_hours' required"
-                " for computing dispersion")
-        start = datetimeutils.parse_datetime(start_str, 'start')
-
-        dest_dir = fires_manager.get_config_value('dispersion', 'dest_dir')
-        if not dest_dir:
-            raise ValueError("Specify directory to save dispersion run output")
-
-        output_dir_name = (fires_manager.get_config_value('dispersion',
-            'output_dir_name') or fires_manager.run_id)
+        start, num_hours = _get_time(fires_manager)
+        dest_dir, output_dir_name = _get_dirs(fires_manager)
 
         # further validation of start and num_hours done in 'run'
         dispersion_info = disperser.run(fires_manager.fires, start,
@@ -85,3 +57,44 @@ def run(fires_manager):
             **processed_kwargs)
 
     # TODO: add information to fires_manager indicating where to find the hysplit output
+
+
+def _get_module_and_class(model):
+    module_name = "bluesky.dispersers.{}".format(model)
+    logging.debug("Importing %s", module_name)
+    try:
+        module = importlib.import_module(module_name)
+    except ImportError:
+        raise BlueSkyConfigurationError(
+            "Invalid dispersion model: '{}'".format(model))
+
+    klass_name = "{}Dispersion".format(model.upper())
+    logging.debug("Loading class %s", klass_name)
+    try:
+        klass = getattr(module, klass_name)
+    except:
+        # TODO: use more appropriate exception class
+        raise RuntimeError("{} does not define class {}".format(
+            module_name, klass_name))
+
+    return module, klass
+
+def _get_time(fires_manager):
+    start_str = fires_manager.get_config_value('dispersion', 'start')
+    num_hours = fires_manager.get_config_value('dispersion', 'num_hours')
+    if not start_str or not num_hours:
+        raise ValueError("Config settings 'start' and 'num_hours' required"
+            " for computing dispersion")
+    start = datetimeutils.parse_datetime(start_str, 'start')
+    return start, num_hours
+
+
+def _get_dirs(fires_manager):
+    dest_dir = fires_manager.get_config_value('dispersion', 'dest_dir')
+    if not dest_dir:
+        raise ValueError("Specify directory to save dispersion run output")
+
+    output_dir_name = (fires_manager.get_config_value('dispersion',
+        'output_dir_name') or fires_manager.run_id)
+
+    return dest_dir, output_dir_name
