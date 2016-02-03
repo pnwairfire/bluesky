57836d94241be1c88093f5975ce94c212d6eac30
Joel Dubowy <jdubowy@gmail.com>
2016-02-03 13:17:46 -0800

    Upgrade to blueskykml v0.2.8

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "fccsmap>=0.2.0",
         "timeprofile>=0.1.2",
         "plumerise>=0.1.2",
-        "blueskykml>=0.2.7",
+        "blueskykml>=0.2.8",
         "apps-consume4>=4.1.2",
         "pymongo>=3.1.1"
     ],
