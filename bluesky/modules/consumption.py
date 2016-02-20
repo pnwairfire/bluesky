5df2258a6584a55007881d5c083984064143ba4c
Joel Dubowy <jdubowy@gmail.com>
2016-02-19 17:30:58 -0800

    Support skip_failed_fires option in consumption module
    and add test input data

--- bluesky/modules/consumption.py
+++ bluesky/modules/consumption.py
@@ -45,81 +45,86 @@ def run(fires_manager):
     # use it across all fires, or at lesat accross all fuelbeds within
     # a single fire?
     for fire in fires_manager.fires:
-        logging.debug("Consume consumption - fire {}".format(fire.id))
-
-        # TODO: set burn type to 'activity' if fire.fuel_type == 'piles' ?
-        if fire.fuel_type == 'piles':
-            raise ValueError("Consume can't be used for fuel type 'piles'")
-        burn_type = fire.fuel_type
-
-        # TODO: can I run consume on all fuelbeds at once and get per-fuelbed
-        # results?  If it is simply a matter of parsing separated values from
-        # the results, make sure that running all at once produces any performance
-        # gain; if it doesn't, then it might not be worth the trouble
-        for fb in fire.fuelbeds:
-            fuel_loadings_csv_filename = fuel_loadings_manager.generate_custom_csv(
-                fb['fccs_id'])
-
-            fc = consume.FuelConsumption(
-                fccs_file=fuel_loadings_csv_filename) #msg_level=msg_level)
-
-            fb['fuel_loadings'] = fuel_loadings_manager.get_fuel_loadings(fb['fccs_id'], fc.FCCS)
-
-            fc.burn_type = burn_type
-            fc.fuelbed_fccs_ids = [fb['fccs_id']]
-
-            # Note: if we end up running fc on all fuelbeds at once, use lists
-            # for the rest
-            # Note: consume expects area, but disregards it when computing
-            #  consumption values - it produces tons per unit area (acre?), not
-            #  total tons; the consumption values will be multiplied by area, below
-            area = (fb['pct'] / 100.0) * fire.location['area']
-            fc.fuelbed_area_acres = [area]
-            fc.fuelbed_ecoregion = [fire.location['ecoregion']]
-
-            _apply_settings(fc, fire['location'], burn_type)
-            _results = fc.results()
-            if _results:
-                fb['consumption'] = _results['consumption']
-                fb['consumption'].pop('debug', None)
-                fb['heat'] = _results['heat release']
-
-                # multiply each consumption and heat value by area if
-                # output_inits is 'tons_ac',
-                # TODO: multiple by area even if user sets output_units to 'tons',
-                #   because consume doesn't seem to be multiplying by area for us
-                #   even when 'tons' is specified
-                if fc.output_units == 'tons_ac':
-                    datautils.multiply_nested_data(fb["consumption"], area)
-                    datautils.multiply_nested_data(fb["heat"], area)
-
-            else:
-                # TODO: somehow get error information from fc object; when
-                #   you call fc.results() in an error situation, it writes to
-                #   stdout or stderr (?), something like:
-                #
-                #     !!! Error settings problem, the following are required:
-                #            fm_type
-                #   it would be nice to access that error message here and
-                #   include it in the exception message
-                raise RuntimeError("Failed to calculate consumption for fire "
-                    "{} fuelbed {}".format(fire.id, fb['fccs_id']))
+        with fires_manager.fire_failure_handler(fire):
+            _run_fire(msg_level, fuel_loadings_manager, fire)
 
     fires_manager.summarize(
         consumption=datautils.summarize(fires_manager.fires, 'consumption'))
     fires_manager.summarize(
         heat=datautils.summarize(fires_manager.fires, 'heat'))
 
+def _run_fire(msg_level, fuel_loadings_manager, fire):
+    logging.debug("Consume consumption - fire {}".format(fire.id))
+
+    # TODO: set burn type to 'activity' if fire.fuel_type == 'piles' ?
+    if fire.fuel_type == 'piles':
+        raise ValueError("Consume can't be used for fuel type 'piles'")
+    burn_type = fire.fuel_type
+
+    # TODO: can I run consume on all fuelbeds at once and get per-fuelbed
+    # results?  If it is simply a matter of parsing separated values from
+    # the results, make sure that running all at once produces any performance
+    # gain; if it doesn't, then it might not be worth the trouble
+    for fb in fire.fuelbeds:
+        fuel_loadings_csv_filename = fuel_loadings_manager.generate_custom_csv(
+            fb['fccs_id'])
+
+        fc = consume.FuelConsumption(
+            fccs_file=fuel_loadings_csv_filename) #msg_level=msg_level)
+
+        fb['fuel_loadings'] = fuel_loadings_manager.get_fuel_loadings(fb['fccs_id'], fc.FCCS)
+
+        fc.burn_type = burn_type
+        fc.fuelbed_fccs_ids = [fb['fccs_id']]
+
+        # Note: if we end up running fc on all fuelbeds at once, use lists
+        # for the rest
+        # Note: consume expects area, but disregards it when computing
+        #  consumption values - it produces tons per unit area (acre?), not
+        #  total tons; the consumption values will be multiplied by area, below
+        area = (fb['pct'] / 100.0) * fire.location['area']
+        fc.fuelbed_area_acres = [area]
+        fc.fuelbed_ecoregion = [fire.location['ecoregion']]
+
+        _apply_settings(fc, fire['location'], burn_type)
+        _results = fc.results()
+        if _results:
+            fb['consumption'] = _results['consumption']
+            fb['consumption'].pop('debug', None)
+            fb['heat'] = _results['heat release']
+
+            # multiply each consumption and heat value by area if
+            # output_inits is 'tons_ac',
+            # TODO: multiple by area even if user sets output_units to 'tons',
+            #   because consume doesn't seem to be multiplying by area for us
+            #   even when 'tons' is specified
+            if fc.output_units == 'tons_ac':
+                datautils.multiply_nested_data(fb["consumption"], area)
+                datautils.multiply_nested_data(fb["heat"], area)
+
+        else:
+            # TODO: somehow get error information from fc object; when
+            #   you call fc.results() in an error situation, it writes to
+            #   stdout or stderr (?), something like:
+            #
+            #     !!! Error settings problem, the following are required:
+            #            fm_type
+            #   it would be nice to access that error message here and
+            #   include it in the exception message
+            raise RuntimeError("Failed to calculate consumption for fire "
+                "{} fuelbed {}".format(fire.id, fb['fccs_id']))
+
 REQUIRED_LOCATION_FIELDS = ['area', 'ecoregion']
 def _validate_input(fires_manager):
     for fire in fires_manager.fires:
-        if not fire.get('fuelbeds'):
-            raise ValueError("Missing fuelbed data required for computing consumption")
-        if not fire.get('location'):
-            raise ValueError("Missing location data required for computing consumption")
-        if any([not fire.location.get(k) for k in REQUIRED_LOCATION_FIELDS]):
-            raise ValueError("Fire location data must define 'area' and "
-                "'ecoregion' for computing consumption")
-        for fb in fire.fuelbeds:
-            if not fb.get('fccs_id') or not fb.get('pct'):
-                raise ValueError("Each fuelbed must define 'id' and 'pct'")
+        with fires_manager.fire_failure_handler(fire):
+            if not fire.get('fuelbeds'):
+                raise ValueError("Missing fuelbed data required for computing consumption")
+            if not fire.get('location'):
+                raise ValueError("Missing location data required for computing consumption")
+            if any([not fire.location.get(k) for k in REQUIRED_LOCATION_FIELDS]):
+                raise ValueError("Fire location data must define 'area' and "
+                    "'ecoregion' for computing consumption")
+            for fb in fire.fuelbeds:
+                if not fb.get('fccs_id') or not fb.get('pct'):
+                    raise ValueError("Each fuelbed must define 'id' and 'pct'")
