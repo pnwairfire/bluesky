78717dbc02a5183f20a6756aca9a540ea0f0f6ab
Joel Dubowy <jdubowy@gmail.com>
2016-03-01 12:27:25 -0800

    Add filter module

---
+++ bluesky/modules/filter.py
@@ -0,0 +1,23 @@
+"""bluesky.modules.merge"""
+
+__author__ = "Joel Dubowy"
+__copyright__ = "Copyright 2016, AirFire, PNW, USFS"
+
+import logging
+
+__all__ = [
+    'run'
+]
+
+__version__ = "0.1.0"
+
+
+def run(fires_manager):
+    """Merges fires with the same id
+
+    Args:
+     - fires_manager -- bluesky.models.fires.FiresManager object
+    """
+    logging.info("Running filter module")
+    fires_manager.processed(__name__, __version__)
+    fires_manager.filter_fires()
