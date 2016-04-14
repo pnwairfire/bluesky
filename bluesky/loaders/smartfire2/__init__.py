9b44973e98f2274ab71f2fd67e83c4873ef5ebd5
Joel Dubowy <jdubowy@gmail.com>
2016-04-14 14:52:51 -0700

    Add skeleton SF2 file loading class

---
+++ bluesky/loaders/smartfire2/__init__.py
@@ -0,0 +1,5 @@
+"""bluesky.loaders.smartfire
+"""
+
+__author__ = "Joel Dubowy"
+__copyright__ = "Copyright 2016, AirFire, PNW, USFS"
---
+++ bluesky/loaders/smartfire2/csv.py
@@ -0,0 +1,19 @@
+"""bluesky.loaders.smartfire.csv
+"""
+
+__author__ = "Joel Dubowy"
+__copyright__ = "Copyright 2016, AirFire, PNW, USFS"
+
+
+__all__ = [
+    'FileLoader'
+]
+
+class FileLoader(object):
+
+    def __init__(self, *config):
+        self._config = config
+
+    def load(self):
+        # TODO: implement
+        return []
