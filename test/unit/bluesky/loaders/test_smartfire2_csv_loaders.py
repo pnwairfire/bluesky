0405a1bd51e4dbf7300b3efc5355b202265b70be
Joel Dubowy <jdubowy@gmail.com>
2016-04-15 16:22:54 -0700

    Add empty unit tests file for SF2 csv loaders
    currently only file loader

---
+++ test/unit/bluesky/loaders/test_smartfire2_csv_loaders.py
@@ -0,0 +1,12 @@
+"""Unit tests for smartfire2 csv loaders defined in bluesky.loaders.smartfire2.csv
+"""
+
+import datetime
+
+from freezegun import freeze_time
+from py.test import raises
+
+from bluesky.loaders.smartfire2 import csv as sf2csv
+
+class TestSmartfire2CsvFileLoader(object):
+    pass
