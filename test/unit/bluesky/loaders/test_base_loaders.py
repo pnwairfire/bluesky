1df5157319cc530563412188465a193db3d19e01
Joel Dubowy <jdubowy@gmail.com>
2016-04-15 22:26:23 -0700

    Fill in unit tests

--- test/unit/bluesky/loaders/test_base_loaders.py
+++ test/unit/bluesky/loaders/test_base_loaders.py
@@ -2,10 +2,13 @@
 """
 
 import datetime
+import os
+import tempfile
 
 from freezegun import freeze_time
 from py.test import raises
 
+from bluesky.exceptions import BlueSkyConfigurationError
 from bluesky.loaders import BaseLoader, BaseFileLoader
 
 class TestBaseLoader(object):
@@ -34,22 +37,47 @@ class TestBaseLoader(object):
 
 class TestBaseFileLoader(object):
 
+    def setup(self):
+        self._temp_dir = tempfile.mkdtemp()
+
+
     def test_file_doesnt_exist(self):
-        # TODO: make sure exception is raised
-        pass
+        filename = os.path.join(self._temp_dir, "fires.json")
+        with raises(BlueSkyConfigurationError) as e_info:
+            l = BaseFileLoader(file=filename)
+        assert e_info.value.message == 'File {} does not exist'.format(filename)
 
     def test_file_exists(self):
-        # TODO: make sure no exception and self._filename is set appropriately
-        pass
+        filename = os.path.join(self._temp_dir, "fires.json")
+        with open(filename, 'w') as f:
+            f.write('{}')
+        l = BaseFileLoader(file=filename)
+        assert l._filename == filename
 
+    @freeze_time("2016-01-14")
     def test_file_exists_with_datetime_codes(self):
-        # TODO: make sure no exception and self._filename is set appropriately
-        pass
+        filename = os.path.join(self._temp_dir, "fires%Y%m%d.json")
+        with open(filename, 'w') as f:
+            f.write('{}')
+        l = BaseFileLoader(file=filename)
+        assert l._filename == filename
 
+    @freeze_time("2016-01-14")
     def test_file_exists_with_defined_date_in_name(self):
-        # TODO: make sure no exception and self._filename is set appropriately
-        pass
+        config_filename = os.path.join(self._temp_dir, "fires%Y%m%d.json")
+        real_filename = os.path.join(self._temp_dir, "fires20160114.json")
+        with open(real_filename, 'w') as f:
+            f.write('{}')
+        l = BaseFileLoader(file=config_filename)
+        assert l._filename == real_filename
 
+    @freeze_time("2016-01-14")
     def test_file_exists_with_different_defined_date_in_name(self):
-        # TODO: make sure exception is raised
-        pass
+        config_filename = os.path.join(self._temp_dir, "fires%Y%m%d.json")
+        interpolated_filename = os.path.join(self._temp_dir, "fires20160114.json")
+        real_filename = os.path.join(self._temp_dir, "fires20151214.json")
+        with open(real_filename, 'w') as f:
+            f.write('{}')
+        with raises(BlueSkyConfigurationError) as e_info:
+            l = BaseFileLoader(file=config_filename)
+        assert e_info.value.message == 'File {} does not exist'.format(interpolated_filename)
