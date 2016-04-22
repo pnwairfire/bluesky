7fa67b253c7c08f8420ec7de628bb69aa8171277
Joel Dubowy <jdubowy@gmail.com>
2016-04-21 22:00:30 -0700

    Refactor find_with_datetime to handle undefined date_time

--- bluesky/fileutils.py
+++ bluesky/fileutils.py
@@ -11,13 +11,14 @@ def find_with_datetime(file_or_dir_name, date_time):
     if not file_or_dir_name:
         raise BlueSkyConfigurationError('File not specified')
 
-    # if file does exist with, try filling in datetime codes
-    if not os.path.exists(file_or_dir_name):
+    # if file does not exist, and if date_time is defined, then try filling
+    # in datetime codes before checking existence again
+    if date_time and not os.path.exists(file_or_dir_name):
         file_or_dir_name = date_time.strftime(file_or_dir_name)
 
-        # if it still doesn't exist, raise exception
-        if not os.path.isfile(file_or_dir_name):
-            raise BlueSkyConfigurationError('File/dir {} does not exist'.format(
-                file_or_dir_name))
+    # if it still doesn't exist, raise exception
+    if not os.path.isfile(file_or_dir_name):
+        raise BlueSkyConfigurationError('File/dir {} does not exist'.format(
+            file_or_dir_name))
 
     return file_or_dir_name
