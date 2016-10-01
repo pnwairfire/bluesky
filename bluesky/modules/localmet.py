b9ad9c1517249f73f51d371c00397cb8449f7bf2
Joel Dubowy <jdubowy@gmail.com>
2016-09-30 17:24:37 -0700

    Fix getting of lat/lng for growth window in localmet module

--- bluesky/modules/localmet.py
+++ bluesky/modules/localmet.py
@@ -14,6 +14,7 @@ import os
 from met.arl.arlprofiler import ArlProfiler
 
 from bluesky.datetimeutils import parse_datetimes, parse_utc_offset
+from bluesky.locationutils import LatLng
 
 __all__ = [
     'run'
@@ -41,30 +42,12 @@ def run(fires_manager):
             if not fire.get('growth'):
                 raise ValueError("Missing growth data required for localmet")
             for g in fire['growth']:
-                lat,lng = _fire_lat_lng(g)
+                latlng = LatLng(growth.get('location'))
                 # parse_utc_offset makes sure utc offset is defined and valid
                 utc_offset = parse_utc_offset(g.get('location', {}).get('utc_offset'))
                 tw = parse_datetimes(g, 'start', 'end')
-                g['localmet'] = arl_profiler.profile(lat, lng, tw['start'],
+                g['localmet'] = arl_profiler.profile(latlng.latitude,
+                    latlng.longitude, tw['start'],
                     tw['end'], utc_offset)
 
     # fires_manager.summarize(...)
-
-def _fire_lat_lng(growth):
-    if not growth.get('location'):
-        raise ValueError("Missing location data required for localmet")
-    if growth['location'].get('geojson'):
-        # TODO: get centroid; also, can'st assume 3-deep nested
-        # array (it's 3-deep for MultiPolygon, but not necessarily other shape types)
-        lat = growth['location']['geojson']['coordinates'][0][0][0][1]
-        lng = growth['location']['geojson']['coordinates'][0][0][0][0]
-    elif growth['location'].get('latitude') and growth['location'].get('longitude'):
-        lat = growth['location']['latitude']
-        lng = growth['location']['longitude']
-    elif growth['location'].get('shape_file'):
-        raise NotImplementedError("Importing of shape data from file not implemented")
-    else:
-        raise ValueError(
-            "Insufficient location data required for looking up lcalmet data")
-
-    return (lat, lng)
