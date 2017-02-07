6353d5ac02440304176ee7efa8e5137f099e85f3
Joel Dubowy <jdubowy@gmail.com>
2017-02-07 10:10:50 -0800

    Implement alternate ecoregion lookup method using ogr
    It's much faster. I need to do some time test to compare it
    against the  Fiona + shapely implementation.
    
    added unit tests

--- bluesky/ecoregion/lookup.py
+++ bluesky/ecoregion/lookup.py
@@ -9,6 +9,7 @@ import os
 
 #import shapefile
 import fiona
+import ogr
 from shapely import geometry
 
 from bluesky.exceptions import BlueSkyGeographyValueError
@@ -20,15 +21,25 @@ __all__ = ['lookup']
 ECOREGION_SHAPEFILE = os.path.join(os.path.dirname(__file__), 'data', '3ecoregions.shp')
 # ACCEPTED_VALUES = ["western","southern","boreal"]
 
-def lookup_ecoregion(lat, lng):
-    """Looks up ecoregion from lat/lng
+
+## Fiona + shapely
+
+SHAPEFILE_RECORDS = []
+with fiona.open(ECOREGION_SHAPEFILE) as shapes:
+    SHAPEFILE_RECORDS = [s for s in shapes]
+
+def _validate_lat_lng(lat, lng):
+    if abs(lat) > 90.0 or abs(lng) > 180.0:
+        raise BlueSkyGeographyValueError(
+            "Invalid lat,lng: {},{}".format(lat, lng))
+
+def lookup_ecoregion_shapely(lat, lng):
+    """Looks up ecoregion from lat/lng using shapely + fiona
 
     Note: If a fire's location is defined as a polygon, it's the calling
       code's responsibility to pick a representative lat/lng.
     """
-    if abs(lat) > 90.0 or abs(lng) > 180.0:
-        raise BlueSkyGeographyValueError(
-            "Invalid lat,lng: {},{}".format(lat, lng))
+    _validate_lat_lng(lat, lng)
 
     # TODO: Handle exceptions here or in calling code ?
 
@@ -37,9 +48,32 @@ def lookup_ecoregion(lat, lng):
     point = geometry.Point(lng, lat) # longitude, latitude
 
     ecoregion = None
-    with fiona.open(ECOREGION_SHAPEFILE) as shapes:
-        for shapefile_record in shapes:
-            shape = geometry.asShape(shapefile_record['geometry'])
-            #import pdb;pdb.set_trace()
-            if shape.contains(point):
-                return shapefile_record['properties']['DOMAIN']
+    for sr in SHAPEFILE_RECORDS:
+        shape = geometry.asShape(sr['geometry'])
+        #import pdb;pdb.set_trace()
+        if shape.contains(point):
+            return sr['properties']['DOMAIN']
+
+
+## Ogr
+
+DS_IN = ogr.GetDriverByName('ESRI Shapefile').Open(ECOREGION_SHAPEFILE)
+
+def lookup_ecoregion_ogr(lat, lng):
+    """Looks up ecoregion from lat/lng using gdal's ogr binding
+
+    Note: If a fire's location is defined as a polygon, it's the calling
+      code's responsibility to pick a representative lat/lng.
+    """
+    _validate_lat_lng(lat, lng)
+
+    lyr_in = DS_IN.GetLayer(0)
+    idx_reg = lyr_in.GetLayerDefn().GetFieldIndex("DOMAIN")
+    pt = ogr.Geometry(ogr.wkbPoint)
+    pt.SetPoint_2D(0, lng, lat)
+    lyr_in.SetSpatialFilter(pt)
+    for feat_in in lyr_in:
+        # roughly subsets features, instead of go over everything
+        ply = feat_in.GetGeometryRef()
+        if ply.Contains(pt):
+            return feat_in.GetFieldAsString(idx_reg)
--- test/unit/bluesky/ecoregion/test_lookup.py
+++ test/unit/bluesky/ecoregion/test_lookup.py
@@ -3,14 +3,14 @@
 
 from py.test import raises
 
-from bluesky.ecoregion.lookup import lookup_ecoregion
+from bluesky.ecoregion.lookup import lookup_ecoregion_shapely, lookup_ecoregion_ogr
 from bluesky.exceptions import BlueSkyGeographyValueError
 
-class TestEcoregionLookup(object):
+class TestLookupEcoregion(object):
 
     def test_valid(self):
-        assert 'western' == lookup_ecoregion(45, -118)
-        assert 'southern' == lookup_ecoregion(32, -88)
+        assert 'western' == lookup_ecoregion_shapely(45, -118)
+        assert 'southern' == lookup_ecoregion_shapely(32, -88)
         # TODO: add a boreal example
 
 
@@ -18,13 +18,36 @@ class TestEcoregionLookup(object):
         # TODO: raise specific exception in lookup_ecoregion and
         #   update this code with that exception class
         with raises(BlueSkyGeographyValueError) as e_info:
-            lookup_ecoregion(28, -182)
+            lookup_ecoregion_shapely(28, -182)
 
         with raises(BlueSkyGeographyValueError) as e_info:
-            lookup_ecoregion(99, -122)
+            lookup_ecoregion_shapely(99, -122)
 
         # water
-        assert None == lookup_ecoregion(28, -88)
+        assert None == lookup_ecoregion_shapely(28, -88)
 
         # on land but outside of shapefile area
-        assert None == lookup_ecoregion(19, -100)
+        assert None == lookup_ecoregion_shapely(19, -100)
+
+class TestLookupEcoregionOgr(object):
+
+    def test_valid(self):
+        assert 'western' == lookup_ecoregion_ogr(45, -118)
+        assert 'southern' == lookup_ecoregion_ogr(32, -88)
+        # TODO: add a boreal example
+
+
+    def test_invalid(self):
+        # TODO: raise specific exception in lookup_ecoregion and
+        #   update this code with that exception class
+        with raises(BlueSkyGeographyValueError) as e_info:
+            lookup_ecoregion_ogr(28, -182)
+
+        with raises(BlueSkyGeographyValueError) as e_info:
+            lookup_ecoregion_ogr(99, -122)
+
+        # water
+        assert None == lookup_ecoregion_ogr(28, -88)
+
+        # on land but outside of shapefile area
+        assert None == lookup_ecoregion_ogr(19, -100)
