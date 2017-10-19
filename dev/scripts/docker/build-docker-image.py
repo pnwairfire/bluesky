dae3011140c5924b79688656bd3397b6725c474d
Joel Dubowy <jdubowy@gmail.com>
2017-10-19 09:47:30 -0700

    Prompt to confirm or override tag version, and other refactoring

--- dev/scripts/docker/build-docker-image.py
+++ dev/scripts/docker/build-docker-image.py
@@ -4,6 +4,7 @@ __author__ = "Joel Dubowy"
 
 import logging
 import os
+import re
 import subprocess
 import sys
 import traceback
@@ -47,25 +48,33 @@ def _call(cmd_args):
             "Command '{}' returned error code {}".format(' '.join(cmd_args), r),
             exit_code=r)
 
-def _build(args):
+def build(image_name):
     dockerfile_pathname = os.path.join(REPO_ROOT_DIR, 'Dockerfile')
-    _call(['docker','build', '-t', args.image_name, REPO_ROOT_DIR])
+    _call(['docker','build', '-t', image_name, REPO_ROOT_DIR])
 
-def _tag(args):
-    _call(['docker', 'tag', args.image_name, 'pnwairfire/bluesky:latest'])
-    _call(['docker', 'tag', args.image_name, 'pnwairfire/bluesky:v' + __version__])
+def tag(image_name):
+    tag_version = None
+    while not tag_version or not re.compile('^v\d+\.\d+\.\d+$').match(tag_version):
+        if tag_version:
+            sys.stdout.write("Enter a tag version of the form 'v{}' or press"
+                " enter to use the default\n".format(__version__))
+        sys.stdout.write('Tag version [v{}]: '.format(__version__))
+        tag_version = input().strip() or 'v' + __version__
+    _call(['docker', 'tag', image_name, 'pnwairfire/bluesky:latest'])
+    _call(['docker', 'tag', image_name, 'pnwairfire/bluesky:' + tag_version])
+    return tag_version
 
-def _push(args):
-    sys.stdout.write('Push to hub.docker.com?: [y/N]: ')
+def upload(tag_version):
+    sys.stdout.write('Upload to hub.docker.com?: [y/N]: ')
     r = input().strip()
     if r and r.lower() in ('y', 'yes'):
         logging.info("Pushing to docker up")
         _call(['docker', 'push', 'pnwairfire/bluesky:latest'])
-        _call(['docker', 'push', 'pnwairfire/bluesky:v' + __version__])
+        _call(['docker', 'push', 'pnwairfire/bluesky:' + tag_version])
 
 if __name__ == "__main__":
     parser, args = afscripting.args.parse_args(REQUIRED_ARGS, OPTIONAL_ARGS,
         epilog=EXAMPLES_STR)
-    _build(args)
-    _tag(args)
-    _push(args)
+    build(args.image_name)
+    tag_version = tag(args.image_name)
+    upload(tag_version)
