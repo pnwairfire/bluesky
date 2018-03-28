e319d106e7dd38b01d76aacc3ded7d3d25081a8a
Joel Dubowy <jdubowy@gmail.com>
2018-03-28 12:45:07 -0700

    Update firespider marshal unit tests

--- test/unit/bluesky/loaders/test_firespider.py
+++ test/unit/bluesky/loaders/test_firespider.py
@@ -91,47 +91,52 @@ MARSHALED = [
 class TestBaseFireSpiderLoader(object):
 
     def setup(self):
-        self._loader = firespider.BaseFireSpiderLoader()
+        pass
+
+    def _call_marshal(self, fires, start=None, end=None):
+        loader = firespider.JsonApiLoader(endpoint="sdf",
+            start=start, end=end)
+        return loader._marshal(fires)
 
     def test_marshal_no_start_end(self):
-        assert MARSHALED == self._loader._marshal(
+        assert MARSHALED == self._call_marshal(
             copy.deepcopy(FT_FIRES))
 
     def test_marshal_w_start(self):
         expected = copy.deepcopy(MARSHALED)
 
         # start is before all growth windows
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start=datetime.datetime(2015,8,8,7,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start="2015-08-08T07:00:00Z")
 
         # start is in the middle of first growth window
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start=datetime.datetime(2015,8,9,14,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start="2015-08-09T14:00:00Z")
 
         # start is in the middle of second growth window
         expected[0]['growth'].pop(0)
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start=datetime.datetime(2015,8,10,14,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start="2015-08-10T14:00:00Z")
 
         # start is after all growth windows
         expected[0]['growth'].pop(0)
         assert expected[0]['growth'] == [] # for sanity
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start=datetime.datetime(2015,8,12,14,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start="2015-08-12T14:00:00Z")
 
@@ -139,37 +144,37 @@ class TestBaseFireSpiderLoader(object):
         expected = copy.deepcopy(MARSHALED)
 
         # end is after all growth windows
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             end=datetime.datetime(2015,8,12,7,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             end="2015-08-12T07:00:00Z")
 
         # start is in the middle of second growth window
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             end=datetime.datetime(2015,8,10,14,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             end="2015-08-10T14:00:00Z")
 
         # start is in the middle of first growth window
         expected[0]['growth'].pop()
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             end=datetime.datetime(2015,8,9,14,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             end="2015-08-09T14:00:00Z")
 
         # start is before all growth windows
         expected[0]['growth'].pop()
         assert expected[0]['growth'] == [] # for sanity
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             end=datetime.datetime(2015,8,8,14,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             end="2015-08-08T14:00:00Z")
 
@@ -177,32 +182,32 @@ class TestBaseFireSpiderLoader(object):
         expected = copy.deepcopy(MARSHALED)
 
         # start/end are outside of growth windows
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start=datetime.datetime(2015,8,8,7,0,0),
             end=datetime.datetime(2015,8,12,7,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start="2015-08-08T07:00:00Z",
             end="2015-08-12T07:00:00Z")
 
         # start/end are inside, but including all growth windows
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start=datetime.datetime(2015,8,9,14,0,0),
             end=datetime.datetime(2015,8,10,14,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start="2015-08-09T14:00:00Z",
             end="2015-08-10T14:00:00Z")
 
         # start/end are inside first growth window
         expected[0]['growth'].pop()
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start=datetime.datetime(2015,8,9,14,0,0),
             end=datetime.datetime(2015,8,10,1,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start="2015-08-09T14:00:00Z",
             end="2015-08-10T01:00:00Z")
@@ -210,24 +215,24 @@ class TestBaseFireSpiderLoader(object):
         # exclude all growth windows
         expected[0]['growth'].pop()
         assert expected[0]['growth'] == [] # for sanity
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start=datetime.datetime(2015,8,7,14,0,0),
             end=datetime.datetime(2015,8,8,1,0,0))
-        assert expected == self._loader._marshal(
+        assert expected == self._call_marshal(
             copy.deepcopy(FT_FIRES),
             start="2015-08-07T14:00:00Z",
             end="2015-08-08T01:00:00Z")
 
 
         with raises(BlueSkyConfigurationError) as e_info:
-            self._loader._marshal(
+            self._call_marshal(
                 copy.deepcopy(FT_FIRES),
                 start=datetime.datetime(2015,8,7,14,0,0),
                 end=datetime.datetime(2015,8,4,1,0,0))
         assert e_info.value.args[0] == firespider.BaseFireSpiderLoader.START_AFTER_END_ERROR_MSG
         with raises(BlueSkyConfigurationError) as e_info:
-            self._loader._marshal(
+            self._call_marshal(
                 copy.deepcopy(FT_FIRES),
                 start="2015-08-07T14:00:00Z",
                 end="2015-08-04T01:00:00Z")
