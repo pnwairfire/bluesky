4d402df936c69999887da18f9a486556972b1ffe
Joel Dubowy <jdubowy@gmail.com>
2018-06-21 14:00:32 -0700

    Support rerunning FIS, SF2, or any other daily runs

--- dev/scripts/rerun-daily-FIS-output.sh
+++ dev/scripts/rerun-daily-FIS-output.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 4 ]
+if [ $# -lt 5 ]
   then
     echo ""
-    echo "Usage: $0 <host_output_root_dir> <init_time> <mount_local_code> <module_1> [optional_module_2] [...]"
+    echo "Usage: $0 <host_output_root_dir> <domain> <init_time> <mount_local_code> <module_1> [optional_module_2] [...]"
     echo ""
     echo "Examples:"
-    echo "  $0 $HOME/code/pnwairfire-bluesky/tmp/ 2018062100 TRUE visualization"
-    echo "  $0 /data/bluesky-pipeline/output/standard/PNW4km-FS-FIS/ 2018062100 FALSE dispersion visualization"
+    echo "  $0 $HOME/code/pnwairfire-bluesky/tmp/ PNW4km-FireSpider-FIS 2018062100 TRUE visualization"
+    echo "  $0 /data/bluesky-pipeline/output/standard/PNW4km-FS-FIS/ PNW4km-FireSpider-FIS 2018062100 FALSE dispersion visualization"
     echo ""
     exit 1
 fi
 
 HOST_OUTPUT_ROOT_DIR=$(echo $1 | sed 's:/*$::')
-INIT_TIME=$2
-MOUNT_LOCAL=$3
+DOMAIN=$2
+INIT_TIME=$3
+MOUNT_LOCAL=$4
 
 MODULES_STR=""
-while [ $# -gt 3 ]
+while [ $# -gt 4 ]
 do
-  MODULES_STR=$MODULES_STR' '$4
+  MODULES_STR=$MODULES_STR' '$5
   shift
 done
 
@@ -37,7 +38,7 @@ else
 fi
 
 BSP_CMD="docker run -ti --rm"
-BSP_CMD=$BSP_CMD' -v '$HOST_OUTPUT_ROOT_DIR/$INIT_TIME'/forecast:/data/bluesky-daily/output/PNW4km-FireSpider-FIS/'$INIT_TIME'/'
+BSP_CMD=$BSP_CMD' -v '$HOST_OUTPUT_ROOT_DIR/$INIT_TIME'/forecast:/data/bluesky-daily/output/'$DOMAIN'/'$INIT_TIME'/'
 if [ $MOUNT_LOCAL = 'TRUE' ]; then
     BSP_CMD=$BSP_CMD' -v '$HOME'/code/pnwairfire-bluesky/:/bluesky/'
     BSP_CMD=$BSP_CMD' -v '$HOME'/code/pnwairfire-blueskykml/:/blueskykml/'
@@ -48,8 +49,8 @@ fi
 BSP_CMD=$BSP_CMD' -w /bluesky/ bluesky'
 BSP_CMD=$BSP_CMD' bsp --log-level=DEBUG'
 BSP_CMD=$BSP_CMD' -B statuslogging.enabled=false'
-BSP_CMD=$BSP_CMD' -i /data/bluesky-daily/output/PNW4km-FireSpider-FIS/'$INIT_TIME'/output-indented.json'
-BSP_CMD=$BSP_CMD' -o /data/bluesky-daily/output/PNW4km-FireSpider-FIS/'$INIT_TIME'/output-rerun.json'
+BSP_CMD=$BSP_CMD' -i /data/bluesky-daily/output/'$DOMAIN'/'$INIT_TIME'/output-indented.json'
+BSP_CMD=$BSP_CMD' -o /data/bluesky-daily/output/'$DOMAIN'/'$INIT_TIME'/output-rerun.json'
 BSP_CMD=$BSP_CMD' '$MODULES_STR
 
 echo "About to run:  $BSP_CMD"
