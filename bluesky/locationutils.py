c7b4438936a792ef08f79a0db6b0f379146ecaf8
Joel Dubowy <jdubowy@gmail.com>
2019-05-03 13:22:06 -0700

    Refactor locationutils.LatLng to accept active area, specified point, or perimeter objects as input
    And updated and added unit tests

--- bluesky/locationutils.py
+++ bluesky/locationutils.py
@@ -5,20 +5,25 @@ __author__ = "Joel Dubowy"
 from geoutils.geojson import get_centroid
 
 
-INVALID_ACTIVE_AREA_INFO = "Invalid active area data required for determining single, representative lat/lng"
-MISSING_LOCATION_INFO_FOR_ACTIVE_AREA = "Missing location information for active area"
-MISSING_OR_INVALID_LAT_LNG_FOR_SPECIFIED_POINT = "Missing or invalid lat,lng for specified point"
-MISSING_OR_INVALID_COORDINATES_FOR_PERIMETER = "Missing or invalid coodinates for active area perimeter"
+INVALID_LOCATION_DATA = ("Invalid location data required for"
+    " determining single, representative lat/lng")
+MISSING_LOCATION_INFO = ("Missing location information for"
+    " determining single, representative lat/lng")
+
+MISSING_OR_INVALID_LAT_LNG_FOR_SPECIFIED_POINT = (
+    "Missing or invalid lat,lng for specified point")
+MISSING_OR_INVALID_COORDINATES_FOR_PERIMETER = (
+    "Missing or invalid coodinates for active area perimeter")
 
 class LatLng(object):
     """Determines single lat,lng coordinate best representing given
     active area information
     """
 
-    def __init__(self, active_area):
-        if not isinstance(active_area, dict):
-            raise ValueError(INVALID_ACTIVE_AREA_INFO)
-        self._active_area = active_area
+    def __init__(self, location_data):
+        if not isinstance(location_data, dict):
+            raise ValueError(INVALID_LOCATION_DATA)
+        self._location_data = location_data
         self._compute()
 
     @property
@@ -30,34 +35,54 @@ class LatLng(object):
         return self._longitude
 
     def _compute(self):
-        if 'specified_points' in self._active_area:
-            try:
-                geo_data = {
-                    "type": 'MultiPoint',
-                    'coordinates': [[float(p['lng']), float(p['lat'])]
-                        for p in self._active_area['specified_points']]
-                }
-            except:
-                raise ValueError(MISSING_OR_INVALID_LAT_LNG_FOR_SPECIFIED_POINT)
+        if set(['lat', 'lng']).issubset(self._location_data):
+            self._set_from_specified_point(self._location_data)
 
+        elif 'specified_points' in self._location_data:
+            if len(self._location_data['specified_points']) == 1:
+                self._set_from_specified_point(
+                    self._location_data['specified_points'][0])
+
+            else:
+                try:
+                    coordinates = [[float(p['lng']), float(p['lat'])]
+                        for p in self._location_data['specified_points']]
+                except:
+                    raise ValueError(MISSING_OR_INVALID_LAT_LNG_FOR_SPECIFIED_POINT)
+                self._compute_from_geo_data('MultiPoint', coordinates)
+
+        elif 'perimeter' in self._location_data:
+            self._compute_from_geo_data('Polygon',
+                [self._location_data['perimeter'].get('polygon')])
+
+        elif 'polygon' in self._location_data:
+            self._compute_from_geo_data('Polygon',
+                [self._location_data['polygon']])
+
+        else:
+            raise ValueError(MISSING_LOCATION_INFO)
+
+    def _compute_from_geo_data(self, geo_type, coordinates):
+        try:
+            geo_data = {
+                "type": geo_type,
+                'coordinates': coordinates
+            }
             coordinate = get_centroid(geo_data)
             self._latitude = coordinate[1]
             self._longitude = coordinate[0]
 
-        elif 'perimeter' in self._active_area:
-            try:
-                geo_data = {
-                    "type": 'Polygon',
-                    'coordinates': [
-                        self._active_area['perimeter']['polygon']
-                    ]
-                }
-                coordinate = get_centroid(geo_data)
-            except:
-                raise ValueError(MISSING_OR_INVALID_COORDINATES_FOR_PERIMETER)
+        except:
+            error_msg = (MISSING_OR_INVALID_COORDINATES_FOR_PERIMETER
+                if geo_type == 'Polygon'
+                else MISSING_OR_INVALID_LAT_LNG_FOR_SPECIFIED_POINT)
+            raise ValueError(error_msg)
 
-            self._latitude = coordinate[1]
-            self._longitude = coordinate[0]
-
-        else:
-            raise ValueError(MISSING_LOCATION_INFO_FOR_ACTIVE_AREA)
+    def _set_from_specified_point(self, specified_point):
+        """Sets lat and lng from specified point, casting values to float
+        """
+        try:
+            self._latitude = float(specified_point['lat'])
+            self._longitude = float(specified_point['lng'])
+        except:
+            raise ValueError(MISSING_OR_INVALID_LAT_LNG_FOR_SPECIFIED_POINT)
--- test/unit/bluesky/test_locationutils.py
+++ test/unit/bluesky/test_locationutils.py
@@ -9,18 +9,18 @@ from bluesky import  locationutils
 
 class TestLatLng(object):
 
-    def test_invalid_active_area(self):
+    def test_invalid_location_data(self):
         with raises(ValueError) as e_info:
             locationutils.LatLng(None)
-        assert e_info.value.args[0] == locationutils.INVALID_ACTIVE_AREA_INFO
+        assert e_info.value.args[0] == locationutils.INVALID_LOCATION_DATA
 
         with raises(ValueError) as e_info:
             locationutils.LatLng(1)
-        assert e_info.value.args[0] == locationutils.INVALID_ACTIVE_AREA_INFO
+        assert e_info.value.args[0] == locationutils.INVALID_LOCATION_DATA
 
         with raises(ValueError) as e_info:
             locationutils.LatLng("SDFSDF")
-        assert e_info.value.args[0] == locationutils.INVALID_ACTIVE_AREA_INFO
+        assert e_info.value.args[0] == locationutils.INVALID_LOCATION_DATA
 
     def test_no_location_info(self):
         active_area = {
@@ -29,9 +29,12 @@ class TestLatLng(object):
         }
         with raises(ValueError) as e_info:
             locationutils.LatLng(active_area)
-        assert e_info.value.args[0] == locationutils.MISSING_LOCATION_INFO_FOR_ACTIVE_AREA
+        assert e_info.value.args[0] == locationutils.MISSING_LOCATION_INFO
 
-    def test_specified_points_missing_lat_lng_info(self):
+
+class TestLatLngActiveArea(object):
+
+    def test_active_area_with_specified_points_missing_lat_lng_info(self):
         active_area = {
             "start": "2014-05-27T17:00:00",
             "end": "2014-05-28T17:00:00",
@@ -66,7 +69,7 @@ class TestLatLng(object):
             locationutils.LatLng(active_area)
         assert e_info.value.args[0] == locationutils.MISSING_OR_INVALID_LAT_LNG_FOR_SPECIFIED_POINT
 
-    def test_perimeter_invalid_coordinates(self):
+    def test_active_area_with_perimeter_invalid_coordinates(self):
         active_area = {
             "start": "2014-05-27T17:00:00",
             "end": "2014-05-28T17:00:00",
@@ -85,7 +88,7 @@ class TestLatLng(object):
         assert e_info.value.args[0] == locationutils.MISSING_OR_INVALID_COORDINATES_FOR_PERIMETER
 
 
-    def test_single_specified_point(self):
+    def test_active_area_with_single_specified_point(self):
         latlng = locationutils.LatLng({
             "start": "2014-05-27T17:00:00",
             "end": "2014-05-28T17:00:00",
@@ -96,7 +99,7 @@ class TestLatLng(object):
         assert latlng.latitude == 45.0
         assert latlng.longitude == -120.0
 
-    def test_two_specified_points(self):
+    def test_active_area_with_two_specified_points(self):
         latlng = locationutils.LatLng({
             "start": "2014-05-27T17:00:00",
             "end": "2014-05-28T17:00:00",
@@ -108,7 +111,7 @@ class TestLatLng(object):
         assert latlng.latitude == 40.0
         assert latlng.longitude == -120.5
 
-    def test_perimeter(self):
+    def test_active_area_with_perimeter(self):
         latlng = locationutils.LatLng({
             "perimeter": {
                 "polygon": [
@@ -123,7 +126,7 @@ class TestLatLng(object):
         assert latlng.latitude == 2.4
         assert latlng.longitude == 100.4
 
-    def test_specified_points_and_perimeter(self):
+    def test_active_area_with_specified_points_and_perimeter(self):
         latlng = locationutils.LatLng({
             "start": "2014-05-27T17:00:00",
             "end": "2014-05-28T17:00:00",
@@ -143,3 +146,59 @@ class TestLatLng(object):
         })
         assert latlng.latitude == 40.0
         assert latlng.longitude == -120.5
+
+
+class TestLatLngSpecifiedPoint(object):
+
+    def test_specified_point_missing_lat_lng_info(self):
+        sp = {'lng': -120.0}  # missing 'lat'
+        with raises(ValueError) as e_info:
+            locationutils.LatLng(sp)
+        assert e_info.value.args[0] == locationutils.MISSING_LOCATION_INFO
+
+    def test_specified_point_invalid_lat_lng_info(self):
+        sp = {'lat': 23.0, 'lng': "SDF"} # invalid lng
+        with raises(ValueError) as e_info:
+            locationutils.LatLng(sp)
+        assert e_info.value.args[0] == locationutils.MISSING_OR_INVALID_LAT_LNG_FOR_SPECIFIED_POINT
+
+    def test_valid_specified_point(self):
+        latlng = locationutils.LatLng({'area': 34, 'lat': 45.0, 'lng': -120.0})
+        assert latlng.latitude == 45.0
+        assert latlng.longitude == -120.0
+
+
+class TestLatLngPerimeter(object):
+
+    def test_perimeter_missing_coordinates(self):
+        with raises(ValueError) as e_info:
+            locationutils.LatLng({})
+        assert e_info.value.args[0] == locationutils.MISSING_LOCATION_INFO
+
+    def test_perimeter_invalid_coordinates(self):
+        perimeter =  {
+                "polygon": [
+                    ["SDF", 47.43],
+                    [-121.39, 47.43],
+                    [-121.39, 47.40],
+                    [-121.45, 47.40],
+                    [-121.45, 47.43]
+                ]
+            }
+
+        with raises(ValueError) as e_info:
+            locationutils.LatLng(perimeter)
+        assert e_info.value.args[0] == locationutils.MISSING_OR_INVALID_COORDINATES_FOR_PERIMETER
+
+    def test_valid_perimeter(self):
+        latlng = locationutils.LatLng({
+            "polygon": [
+                [100.0, 5.0],
+                [101.0, 0.0],
+                [101.0, 1.0],
+                [100.0, 1.0],
+                [100.0, 5.0]
+            ]
+        })
+        assert latlng.latitude == 2.4
+        assert latlng.longitude == 100.4
