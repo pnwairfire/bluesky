10deedd4e5f92889bc176795ba838a73dc22b0da
Joel Dubowy <jdubowy@gmail.com>
2019-05-10 14:44:20 -0700

    Move code for marshaling v4.0 to v4.1 data from conversion script to importable module and add unit test
    not yet passing

---
+++ bluesky/marshal.py
@@ -0,0 +1,107 @@
+"""bluesky.marshal"""
+
+__author__ = "Joel Dubowy"
+
+
+import copy
+
+
+__all__ = [
+    "Blueskyv4_0To4_1"
+]
+
+
+EXTRA_LOC_FIELDS = (
+    'fuelbeds',
+    'fuelbeds_total_accounted_for_pct',
+    'localmet',
+    'plumerise'
+    # TODO: add fields
+)
+
+TO_COPY_UP = {
+    'consumption',
+    'heat',
+    'emissions',
+    'emissions_details',
+}
+
+class Blueskyv4_0To4_1(object):
+
+    def marshal(self, fires):
+        return [self.marshal_fire(fire) for fire in fires]
+
+    def marshal_fire(self, fire):
+        activity = fire.pop('activity', None) or fire.pop('growth', [])
+        fire['activity'] = []
+        for old_a in activity:
+            loc = old_a.pop('location', {})
+            lat = loc.pop('latitude', None)
+            lng = loc.pop('longitude', None)
+            area = loc.pop('area', None)
+            geojson = loc.pop('geojson', None)
+
+            aa_template = dict(loc)
+            new_a = {'active_areas': []}
+
+            loc_template = {}
+            for k in EXTRA_LOC_FIELDS:
+                val = old_a.pop(k, None)
+                if val:
+                    loc_template[k] = val
+
+            for k in TO_COPY_UP:
+                val = old_a.pop(k, None)
+                if val:
+                    loc_template[k] = val
+                    new_a[k] = val
+                    aa_template[k] = val
+
+            aa_template.update(**old_a)
+
+            if lat is not None and lng is not None:
+                aa = copy.deepcopy(aa_template)
+                aa["specified_points"] = [
+                    dict(loc_template, lat=lat, lng=lng, area=area)
+                ]
+                new_a["active_areas"].append(aa)
+
+            elif geojson:
+                if geojson['type'] == 'Polygon':
+                    aa = copy.deepcopy(aa_template)
+                    aa["perimeter"] = dict(loc_template,
+                        polygon=geojson['coordinates'][0])
+                    new_a["active_areas"].append(aa)
+
+                elif geojson['type'] == 'MultiPolygon':
+                    for p in geojson['coordinates']:
+                        aa = copy.deepcopy(aa_template)
+                        aa["perimeter"] = dict(loc_template, polygon=p[0])
+                        new_a['active_areas'].append(aa)
+
+                elif geojson['type'] == 'MultiPoint' and area:
+                    aa = copy.deepcopy(aa_template)
+                    aa["specified_points"] = [
+                        dict(loc_template,lat=p[0][1],lng=p[0][0],
+                            area=area / len(geojson['coordinates']))
+                        for p in geojson['coordinates']
+                    ]
+                    new_a['active_areas'].append(aa)
+
+                elif geojson['type'] == 'Point' and area:
+                    aa = copy.deepcopy(aa_template)
+                    aa["specified_points"] = [
+                        dict(loc_template,lat=geojson['coordinates'][1],
+                            lng=geojson['coordinates'][0], area=area)
+                    ]
+                    new_a['active_areas'].append(aa)
+
+                else:
+                    raise ValueError("Can't convert fire: %s", fire)
+
+            else:
+                raise ValueError("Can't convert fire: %s", fire)
+
+            fire['activity'].append(new_a)
+
+        return fire
---
+++ test/unit/bluesky/test_marshal.py
@@ -0,0 +1,362 @@
+"""Unit tests for bluesky.loaders.firespider
+"""
+
+import copy
+import datetime
+
+from bluesky import marshal
+
+
+NO_GROWTH_FIRE = {
+    "id": "dc621621-fdc2-4dbd-8178-a6edc77bb632",
+    "source": "GOES-16",
+    "type": "WF",
+}
+NO_GROWTH_FIRE_MARSHALED = {
+    "id": "dc621621-fdc2-4dbd-8178-a6edc77bb632",
+    "source": "GOES-16",
+    "type": "WF"
+}
+
+SIMPLE_MULTI_GROWTH_FIRE = {
+    "id": "dc621621-fdc2-4dbd-8178-a6edc77bb632",
+    "source": "GOES-16",
+    "type": "WF",
+    "growth": [
+        {
+            "location": {
+                "area": 8290342.812,
+                "geojson": {
+                    "type": "Point",
+                    "coordinates": [
+                        -98.675,
+                        23.041
+                    ]
+                },
+                "utc_offset": "-06:00"
+            },
+            "start": "2019-02-13T00:00:00",
+            "end": "2019-02-14T00:00:00"
+        },
+        {
+            "location": {
+                "area": 8290342.812,
+                "latitude": 32.0,
+                "longitude": -120.0,
+                "utc_offset": "-06:00"
+            },
+            "start": "2019-02-13T00:00:00",
+            "end": "2019-02-14T00:00:00"
+        },
+        {
+            "location": {
+                "area": 8290342.812,
+                "geojson": {
+                    "type": "MultiPolygon",
+                    "coordinates": [[[
+                        [-121.4522115,47.4316976],
+                        [-121.3990506,47.4316976],
+                        [-120.3990506,47.4099293],
+                        [-121.4522115,47.4099293],
+                        [-121.4522115,47.4316976]
+                    ]]]
+                },
+                "utc_offset": "-06:00"
+            },
+            "start": "2019-02-13T00:00:00",
+            "end": "2019-02-14T00:00:00"
+        },
+        {
+            "location": {
+                "area": 3214.0,
+                "geojson": {
+                    "type": "Polygon",
+                    "coordinates": [[
+                        [-121.4522115,47.4316976],
+                        [-121.3990506,47.4316976],
+                        [-120.3990506,47.4099293],
+                        [-121.4522115,47.4099293],
+                        [-121.4522115,47.4316976]
+                    ]]
+                },
+                "utc_offset": "-06:00"
+            },
+            "start": "2019-02-13T00:00:00",
+            "end": "2019-02-14T00:00:00"
+        }
+    ]
+}
+SIMPLE_MULTI_GROWTH_FIRE_MARSHALED = {
+    "id": "dc621621-fdc2-4dbd-8178-a6edc77bb632",
+    "source": "GOES-16",
+    "type": "WF",
+    "actvity": [
+        {
+            "active_areas": [
+                {
+                    "start": "2019-02-13T00:00:00",
+                    "end": "2019-02-14T00:00:00",
+                    "frp": 30.14771428571428,
+                    "utc_offset": "-06:00",
+                    "specified_points": [
+                        {
+                            "lat": 23.041,
+                            "lng":  -98.675,
+                            "area": 8290342.812
+                        }
+                    ],
+                    "timeprofile": {
+                        "2019-02-13T18:00:00": {
+                            "area_fraction": 1,
+                            "flaming": 1,
+                            "smoldering": 1,
+                            "residual": 1
+                        }
+                    },
+                    "hourly_frp": {
+                        "2019-02-13T18:00:00": 30.14771428571428
+                    }
+                }
+            ]
+        }
+    ]
+}
+# This is a GOES-16 fire produced by FireSpider v2
+FSV2_GOES16_FIRE = {
+    "id": "dc621621-fdc2-4dbd-8178-a6edc77bb632",
+    "source": "GOES-16",
+    "type": "WF",
+    "growth": [
+        {
+            "location": {
+                "area": 8290342.812,
+                "geojson": {
+                    "type": "Point",
+                    "coordinates": [
+                        -98.675,
+                        23.041
+                    ]
+                },
+                "utc_offset": "-06:00"
+            },
+            "start": "2019-02-13T00:00:00",
+            "end": "2019-02-14T00:00:00",
+            "timeprofile": {
+                "2019-02-13T18:00:00": {
+                    "area_fraction": 1,
+                    "flaming": 1,
+                    "smoldering": 1,
+                    "residual": 1
+                }
+            },
+            "hourly_frp": {
+                "2019-02-13T18:00:00": 30.14771428571428
+            },
+            "frp": 30.14771428571428
+        }
+    ]
+}
+FSV2_GOES16_FIRE_MARSHALED = {
+    "id": "dc621621-fdc2-4dbd-8178-a6edc77bb632",
+    "source": "GOES-16",
+    "type": "WF",
+    "actvity": [
+        {
+            "active_areas": [
+                {
+                    "start": "2019-02-13T00:00:00",
+                    "end": "2019-02-14T00:00:00",
+                    "frp": 30.14771428571428,
+                    "utc_offset": "-06:00",
+                    "specified_points": [
+                        {
+                            "lat": 23.041,
+                            "lng":  -98.675,
+                            "area": 8290342.812
+                        }
+                    ],
+                    "timeprofile": {
+                        "2019-02-13T18:00:00": {
+                            "area_fraction": 1,
+                            "flaming": 1,
+                            "smoldering": 1,
+                            "residual": 1
+                        }
+                    },
+                    "hourly_frp": {
+                        "2019-02-13T18:00:00": 30.14771428571428
+                    }
+                }
+            ]
+        }
+    ]
+}
+
+MULTI_GROWTH_FIRE = {
+    "id": "HMS_sdfsdfsdf",
+    "source": "HMS",
+    "name": "HMS_sdfsdfsdf",
+    "fuel_type": "natural",
+    "type": "wildfire",
+    "growth": [
+        {
+            "start": "2015-08-09T00:00:00",
+            "end": "2015-08-10T00:00:00",
+            "timeprofile": {
+                "2015-08-09T00:00:00": {
+                    'area_fraction': 0.4118236472945892,
+                    'flaming': 0.4118236472945892,
+                    'smoldering': 0.4118236472945892,
+                    'residual': 0.4118236472945892
+                },
+                "2015-08-09T06:00:00":  {
+                    'area_fraction': 0.30661322645290584,
+                    'flaming': 0.30661322645290584,
+                    'smoldering': 0.30661322645290584,
+                    'residual': 0.30661322645290584
+                },
+                "2015-08-10T02:00:00": {
+                    'area_fraction': 0.28156312625250507,
+                    'flaming': 0.28156312625250507,
+                    'smoldering': 0.28156312625250507,
+                    'residual': 0.28156312625250507
+                },
+            },
+            "hourly_frp": {
+                "2015-08-09T00:00:00": 47.333333333333336,
+                "2015-08-09T06:00:00":  45.0,
+                "2015-08-10T02:00:00": 53.166666666666664
+            },
+            "location": {
+                "area": 900,
+                "geojson": {
+                    "type": "MultiPoint",
+                    "coordinates": [
+                        [-120.2, 48.1],
+                        [-120.4, 48.2],
+                        [-120.2, 48.2]
+                    ]
+                },
+                "utc_offset": "-07:00"
+            }
+        },
+        {
+            "start": "2015-08-10T00:00:00",
+            "end": "2015-08-11T00:00:00",
+            "location": {
+                "area": 300,
+                "geojson": {
+                    "type": "MultiPoint",
+                    "coordinates": [[-120.2, 48.1]]
+                },
+                "utc_offset": "-07:00"
+            }
+        }
+    ]
+}
+MULTI_GROWTH_FIRE_MARSHALED =     {
+        "id": "HMS_sdfsdfsdf",
+        "source": "HMS",
+        "name": "HMS_sdfsdfsdf",
+        "fuel_type": "natural",
+        "type": "wildfire",
+        "activity": [
+            {
+                "active_areas": [
+                    {
+                        "start": datetime.datetime(2015,8,9,0,0,0),
+                        "end": datetime.datetime(2015,8,10,0,0,0),
+                        "utc_offset": "-07:00",
+                        "specified_points": [
+                            {
+                                "lat": 48.1,
+                                "lng": -120.2,
+                                "area": 300,
+                            },
+                            {
+                                "lat": 48.2,
+                                "lng": -120.4,
+                                "area": 300,
+                            },
+                            {
+                                "lat": 48.2,
+                                "lng": -120.2,
+                                "area": 300,
+                            }
+                        ],
+                        "timeprofile": {
+                            "2015-08-09T00:00:00": {
+                                'area_fraction': 0.4118236472945892,
+                                'flaming': 0.4118236472945892,
+                                'smoldering': 0.4118236472945892,
+                                'residual': 0.4118236472945892
+                            },
+                            "2015-08-09T06:00:00":  {
+                                'area_fraction': 0.30661322645290584,
+                                'flaming': 0.30661322645290584,
+                                'smoldering': 0.30661322645290584,
+                                'residual': 0.30661322645290584
+                            },
+                            "2015-08-10T02:00:00": {
+                                'area_fraction': 0.28156312625250507,
+                                'flaming': 0.28156312625250507,
+                                'smoldering': 0.28156312625250507,
+                                'residual': 0.28156312625250507
+                            },
+                        },
+                        "hourly_frp": {
+                            "2015-08-09T00:00:00": 47.333333333333336,
+                            "2015-08-09T06:00:00":  45.0,
+                            "2015-08-10T02:00:00": 53.166666666666664
+                        }
+                    },
+                    {
+                        "start": datetime.datetime(2015,8,10,0,0,0),
+                        "end": datetime.datetime(2015,8,11,0,0,0),
+                        "specified_points": [
+                            {
+                                "lat": 48.1,
+                                "lng": 120.2,
+                                "area": 300,
+                                "utc_offset": "-07:00"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+class TestMarshalBlueskyv4_0To4_1(object):
+
+    def setup(self):
+        self.marshaler = marshal.Blueskyv4_0To4_1()
+
+    def test_simple_multi_growth(self):
+        expected = [SIMPLE_MULTI_GROWTH_FIRE_MARSHALED]
+        actual = self.marshaler.marshal([SIMPLE_MULTI_GROWTH_FIRE_MARSHALED])
+        assert actual == expected
+
+    def test_fsv2_goes16_fire(self):
+        expected = [FSV2_GOES16_FIRE_MARSHALED]
+        actual = self.marshaler.marshal([FSV2_GOES16_FIRE])
+        import pdb;pdb.set_trace()
+        assert actual == expected
+
+    def test_multi_growth_fire(self):
+        expected = [MULTI_GROWTH_FIRE_MARSHALED]
+        actual = self.marshaler.marshal([MULTI_GROWTH_FIRE])
+        assert actual == expected
+
+    def test_miltiple_fires(self):
+        expected = [
+            SIMPLE_MULTI_GROWTH_FIRE_MARSHALED,
+            FSV2_GOES16_FIRE_MARSHALED,
+            MULTI_GROWTH_FIRE_MARSHALED,
+        ]
+        actual = self.marshaler.marshal([
+            SIMPLE_MULTI_GROWTH_FIRE,
+            FSV2_GOES16_FIRE,
+            MULTI_GROWTH_FIRE,
+        ])
+        assert actual == expected
