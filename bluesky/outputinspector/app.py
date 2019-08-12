bbdd7cff026f9c758262892e1a2c2e98964b269c
Joel Dubowy <jdubowy@gmail.com>
2019-08-12 10:23:22 -0700

    Set custom title in outputinspector app

--- bluesky/outputinspector/app.py
+++ bluesky/outputinspector/app.py
@@ -9,6 +9,7 @@ import plotly.express as px
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Bluesky Output Inspector"
 
 app.layout = html.Div(children=[
     html.H3(children='Bluesky Output Inspector'),
