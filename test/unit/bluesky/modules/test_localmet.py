0129ce198bd70cba0b8b38e89374dd010b20706e
Joel Dubowy <jdubowy@gmail.com>
2020-07-14 16:18:04 -0700

    Update localmet unit test

--- test/unit/bluesky/modules/test_localmet.py
+++ test/unit/bluesky/modules/test_localmet.py
@@ -5,7 +5,7 @@ __author__ = "Joel Dubowy"
 import datetime
 
 from py.test import raises
-from met.arl import arlbulkprofiler
+from met.arl import arlprofiler
 
 from bluesky.config import defaults, Config
 from bluesky.exceptions import BlueSkyConfigurationError
@@ -74,7 +74,7 @@ _ARLP_ARGS = None
 _ARLP_KWARGS = None
 _ARLP_PROFILE_CALL_ARGS = None
 
-class MockArlBulkProfiler(object):
+class MockArlProfiler(object):
     def __init__(self, *args, **kwargs):
         global _ARLP_ARGS, _ARLP_KWARGS, _ARLP_PROFILE_CALL_ARGS
         _ARLP_ARGS = args
@@ -83,9 +83,10 @@ class MockArlBulkProfiler(object):
 
     def profile(self, *args):
         _ARLP_PROFILE_CALL_ARGS.append(args)
+        return [{}] * len(args[2])
 
 def monkeypatch_arl_profiler(monkeypatch):
-    monkeypatch.setattr(arlbulkprofiler, 'ArlBulkProfiler', MockArlBulkProfiler)
+    monkeypatch.setattr(arlprofiler, 'ArlProfiler', MockArlProfiler)
 
 class TestLocalMetRun(object):
 
@@ -121,11 +122,9 @@ class TestLocalMetRun(object):
         fm.load({
             "fires": []
         })
-        localmet.run(fm)
-
-        assert _ARLP_ARGS == (MET_INFO['files'], )
-        assert _ARLP_KWARGS == {'time_step': defaults._DEFAULTS['localmet']['time_step']}
-        assert _ARLP_PROFILE_CALL_ARGS == []
+        with raises(RuntimeError) as e_info:
+            localmet.run(fm)
+        assert e_info.value.args[0] == localmet.NO_START_OR_END_ERROR_MSG
 
     def test_one_fire(self, reset_config, monkeypatch):
         monkeypatch_arl_profiler(monkeypatch)
@@ -140,6 +139,12 @@ class TestLocalMetRun(object):
         assert _ARLP_ARGS == (MET_INFO['files'], )
         assert _ARLP_KWARGS == {'time_step': defaults._DEFAULTS['localmet']['time_step']}
         assert _ARLP_PROFILE_CALL_ARGS == [
-            (45.0, -119.0, datetime.datetime(2015, 1, 20, 17, 0), datetime.datetime(2015, 1, 21, 17, 0), -7.0),
-            (47.415, -121.42, datetime.datetime(2015, 1, 21, 17, 0), datetime.datetime(2015, 1, 22, 17, 0), -7.0)
+            (
+                datetime.datetime(2015, 1, 21, 0, 0),
+                datetime.datetime(2015, 1, 22, 0, 0),
+                [
+                    {'latitude': 45.0, 'longitude': -119.0},
+                    {'latitude': 47.415, 'longitude': -121.42}
+                ]
+            )
         ]
