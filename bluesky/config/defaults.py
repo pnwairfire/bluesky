c77203aa6bbe3affbd02a48bbc1a9fa2a7ab774e
Joel Dubowy <jdubowy@gmail.com>
2020-07-29 14:47:15 -0700

    Support forcing use of AK FCCS fuelbed map
    Useful if all fires are known to be in AK, but none define 'state'.
    
    The code should be updated to key off of lat/lng rather than 'state' field.

--- bluesky/config/defaults.py
+++ bluesky/config/defaults.py
@@ -90,8 +90,10 @@ _DEFAULTS = {
         "truncation_percentage_threshold": 90.0,
         "truncation_count_threshold": 5,
         # Allow summed fuel percentages to be between 99.5% and 100.5%
-        "total_pct_threshold": 0.5
-
+        "total_pct_threshold": 0.5,
+        # Force use of AK fuelbed lookup; useful if all fires
+        # are known to be in AK, but none define 'state'
+        "use_alaska": False
     },
     "consumption": {
         "fuel_loadings": {},
