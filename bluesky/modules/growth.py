d9d067d607faea908584a2dcaf2a23e73ba0a1ee
BlueSky Pipeline operator <pipeop@bluesky2.eos.ubc.ca>
2020-08-20 20:28:23 -0200

    Fixed issue where AirFire merge removed UBC growth module

---
+++ bluesky/modules/growth.py
@@ -0,0 +1,29 @@
+"""bluesky.modules.growth"""
+
+__author__ = "Tobias Schmidt"
+
+__all__ = [
+    'run'
+]
+
+__version__ = "1.0"
+
+import logging
+import os
+import shutil
+
+from bluesky import io
+from bluesky.config import Config
+from bluesky.persisters.persistence import Persistence
+
+def run(fires_manager):
+    """runs the growth module
+
+    Args:
+     - fires_manager -- bluesky.models.fires.FiresManager object
+    """
+
+    pers = Persistence()
+    pers.run(fires_manager)
+
+    fires_manager.processed(__name__, __version__)
