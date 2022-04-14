47306e66c4dd46ddeb59873a1e8a3bc606878b61
Joel Dubowy <jdubowy@gmail.com>
2022-04-14 10:51:48 -0700

    Update default 1000hr and duff FM values, and add new litter FM defaults

--- bluesky/fuelmoisture/__init__.py
+++ bluesky/fuelmoisture/__init__.py
@@ -5,28 +5,28 @@ from afdatetime.parsing import parse_datetime
 # Default FM profiles from BSF
 MOISTURE_PROFILES = {
     "very_dry": {
-        '1_hr': 4, '10_hr': 6, '100_hr': 8, '1000_hr': 8,
-        'live': 60, 'duff': 25
+        '1_hr': 4, '10_hr': 6, '100_hr': 8, '1000_hr': 10,
+        'live': 60, 'duff': 20, 'litter': 4
     },
-    "dry": {
-        '1_hr': 7, '10_hr': 8, '100_hr': 9, '1000_hr': 12,
-        'live': 80, 'duff': 40
+    "dry":      {
+        '1_hr': 7, '10_hr': 8, '100_hr': 9, '1000_hr': 15,
+        'live': 80, 'duff': 40, 'litter': 10
     },
     "moderate": {
-        '1_hr': 8, '10_hr': 9, '100_hr': 11, '1000_hr': 15,
-        'live': 100, 'duff': 70
+        '1_hr': 8, '10_hr': 9, '100_hr': 11, '1000_hr': 30,
+        'live': 100, 'duff': 75, 'litter': 16
     },
-    "moist": {
-        '1_hr': 10, '10_hr': 12, '100_hr': 12, '1000_hr': 22,
-        'live': 130, 'duff': 150
+    "moist":    {
+        '1_hr': 10, '10_hr': 12, '100_hr': 12, '1000_hr': 35,
+        'live': 130, 'duff': 100, 'litter': 22
     },
-    "wet": {
-        '1_hr': 18, '10_hr': 20, '100_hr': 22, '1000_hr': 31,
-        'live': 180, 'duff': 250
+    "wet":      {
+        '1_hr': 18, '10_hr': 20, '100_hr': 22, '1000_hr': 40,
+        'live': 180, 'duff': 130, 'litter': 30
     },
     "very_wet": {
-        '1_hr': 28, '10_hr': 30, '100_hr': 32, '1000_hr': 75,
-        'live': 300, 'duff': 400
+        '1_hr': 28, '10_hr': 30, '100_hr': 32, '1000_hr': 60,
+        'live': 300, 'duff': 180, 'litter': 40
     },
 }
 
