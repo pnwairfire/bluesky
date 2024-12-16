3a514c3ff628abcee3ffe6c4116476c09f8757b1
Joel Dubowy <jdubowy@gmail.com>
2024-12-16 13:26:11 -0800

    Convert fccs_id's to strings on Fire instantiation

--- bluesky/models/fires.py
+++ bluesky/models/fires.py
@@ -72,6 +72,18 @@ class Fire(dict):
         for i in range(len(self.get('activity', []))):
             self['activity'][i] = ActivityCollection(self['activity'][i])
 
+        # If fuelbeds are defined, make sure fccs_id's are strings.
+        # Note that we aren't using 'self.locations' because it would trigger
+        # validations that shouldn't be run now
+        for a in self.get('activity', []):
+            for aa in a.get('active_areas', []):
+                locs = aa.get('specified_points', []) + (
+                    [aa['perimeter']] if aa.get('perimeter') else [])
+                for loc in locs:
+                    for fb in loc.get('fuelbeds', []):
+                        if fb.get('fccs_id'):
+                            fb['fccs_id'] = str(fb['fccs_id'])
+
     ## Properties
 
     # @property
